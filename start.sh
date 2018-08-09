d6469e9b2eaab4a8ba3651560cd01d696936e966
Zhen Qian <zqian@umich.edu>
2018-08-09 14:09:42 -0400

    put symlink before the gunicorn build

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+if [ -f /secrets/bq_cred.json ]
+then
+  ln -sf /secrets/bq_cred.json /dashboard/dashboard/static/json/bq_cred.json
+fi
+
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn dashboard.wsgi:application \
     --bind 0.0.0.0:5000 \
     --workers 3
-if [ -f /secrets/bq_cred.json ]
-then
-  ln -sf /secrets/bq_cred.json /dashboard/dashboard/static/json/bq_cred.json
-fi
