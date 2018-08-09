263d914bf09c2eb0097200fef3157bc7c5dacdd3
Zhen Qian <zqian@umich.edu>
2018-08-09 13:38:38 -0400

    mounting needed for reading secret for OpenShift deployment

--- start.sh
+++ start.sh
@@ -5,3 +5,7 @@ echo Starting Gunicorn.
 exec gunicorn dashboard.wsgi:application \
     --bind 0.0.0.0:5000 \
     --workers 3
+if [ -f /secrets/bq_cred.json ]
+then
+  ln -sf /secrets/bq_cred.json /dashboard/dashboard/static/json/bq_cred.json
+fi
