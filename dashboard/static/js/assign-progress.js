33631f9f5e8a5f58ee666eab2102555ae96570ea
pushyami <pushyami@umich.edu>
2018-09-24 15:05:24 -0400

    #23, #78 assignment view alignment fixes

--- dashboard/static/js/assign-progress.js
+++ dashboard/static/js/assign-progress.js
@@ -14,20 +14,21 @@ var makeGraph = function () {
 
         var tooltip = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
 
-        var width = 2000,
+        var width = 1500,
             height = 100;
 
         var margin = {
             top: 10,
-            right: 10,
+            right: 30,
             bottom: 30,
-            left: 10
+            left: 30
         };
 
         var svg = d3.select("#chart").append("svg")
             .attr('width', '100%')
             .attr('height', '100%')
             .attr('viewBox', '0 0 ' + width + ' ' + height)
+            .attr("preserveAspectRatio", "xMinYMin")
             .append('g');
 
         width = width - margin.left - margin.right,
@@ -50,7 +51,8 @@ var makeGraph = function () {
             .attr('transform', 'translate(' + 0 + ',' + height + ')')
             .call(d3.axisBottom(xScale)
                 .tickFormat(function(d) {
-                return d+"%"})
+                    return d+"%"
+                })
                 .ticks(20, 's'))
 
 
@@ -83,15 +85,29 @@ var makeGraph = function () {
                 tooltip.transition()
                     .duration(200)
                     .style("opacity", 1);
-                tooltip.html("Assignment: <b>"+(d.name) + "</b><br>" +
-                    "Due at: <b>" + (d.due_date_mod) +"</b><br>"+
-                    "Your grade is: <b>"+((!d.score)?"Not available":(d.score))+ "</b><br>"+
-                    "Total Points Possible: <b>"+(d.points_possible)+"</b><br>"+
-                    "Percentage worth in final grade: <b>"+(d.towards_final_grade)+"%</b><br>"+
-                    ((d.drop_lowest!=0)?"The lowest <b>"+d.drop_lowest+"</b> scores will dropped from this assignment group":'')+
-                    ((d.drop_highest!=0)?"The highest <b>"+d.drop_highest+"</b> scores will dropped from this assignment group":''))
-                    .style("left", (d3.event.pageX) + "px")
-                    .style("top", (d3.event.pageY +20) + "px");
+                var coordinates = d3.mouse(this);
+                if ((width - coordinates[0]) < 50) {
+                    tooltip.html("Assignment: <b>" + (d.name) + "</b><br>" +
+                        "Due at: <b>" + (d.due_date_mod) + "</b><br>" +
+                        "Your grade is: <b>" + ((!d.score) ? "Not available" : (d.score)) + "</b><br>" +
+                        "Total Points Possible: <b>" + (d.points_possible) + "</b><br>" +
+                        "Percentage worth in final grade: <b>" + (d.towards_final_grade) + "%</b><br>" +
+                        ((d.drop_lowest != 0) ? "The lowest <b>" + d.drop_lowest + "</b> scores will dropped from this assignment group" : '') +
+                        ((d.drop_highest != 0) ? "The highest <b>" + d.drop_highest + "</b> scores will dropped from this assignment group" : ''))
+                        .style("left", (d3.event.pageX)-(d3.event.pageY*2) + "px")
+                        .style("top", (d3.event.pageY)-150 + "px");
+
+                } else {
+                    tooltip.html("Assignment: <b>" + (d.name) + "</b><br>" +
+                        "Due at: <b>" + (d.due_date_mod) + "</b><br>" +
+                        "Your grade is: <b>" + ((!d.score) ? "Not available" : (d.score)) + "</b><br>" +
+                        "Total Points Possible: <b>" + (d.points_possible) + "</b><br>" +
+                        "Percentage worth in final grade: <b>" + (d.towards_final_grade) + "%</b><br>" +
+                        ((d.drop_lowest != 0) ? "The lowest <b>" + d.drop_lowest + "</b> scores will dropped from this assignment group" : '') +
+                        ((d.drop_highest != 0) ? "The highest <b>" + d.drop_highest + "</b> scores will dropped from this assignment group" : ''))
+                        .style("left", (d3.event.pageX)+ "px")
+                        .style("top", (d3.event.pageY) + "px");
+                }
             })
             .on("mouseout", function(d) {
                 tooltip.transition()
@@ -104,25 +120,83 @@ var makeGraph = function () {
             .attr("class",function(d){
                 return (d.graded===true)? "progress-graded":"progress-nongraded";
             });
-        bar.append()
 
         perc_so_far = 0;
+        let gradeIndicator = [...new Set(data.map(item => item.graded))];
+        var currentWeekXValue=''
+        var maxPossibleWeekXValue=''
         bar.append("text")
-            .attr("x", function(d) {
+            .attr("x", function(d,i) {
                 var dprev_perc = perc_so_far;
                 var dthis_perc = d.percent_gotten;
                 perc_so_far = perc_so_far + dthis_perc;
-                return (xScale(dprev_perc))
+                xCoordinate=xScale(dprev_perc)
+                if(gradeIndicator.length==2) {
+                    if (d.graded) {
+                        currentWeekXValue = xScale(perc_so_far);;
+                    }
+                    if (data.length == i + 1) {
+                        maxPossibleWeekXValue = xScale(perc_so_far);
+                    }
+                }
+                if(gradeIndicator.length==1 && data.length == i + 1 ){
+                    if(gradeIndicator[0]){
+                        currentWeekXValue=xScale(perc_so_far);
+                    }
+                    if(!gradeIndicator[0]){
+                        maxPossibleWeekXValue = xScale(perc_so_far);
+                    }
+                }
+                return (xCoordinate)
             })
             .attr("dy", "2.4em")
             .attr("dx",".6em")
-            .text(function(d) {
-                if (parseFloat(d.percent_gotten)<1.0){
+            .text(function(d,i) {
+                if (parseFloat(d.percent_gotten)<2.0){
                     return "";
                 }
-                return d.name;
+                if(d.name.length > 5)
+                    return d.name.substring(0,5)+'...';
+                else
+                    return d.name;
             });
-
+            if(currentWeekXValue.length!=0) {
+                var currentLine = svg.append("g")
+                currentLine.append('line')
+                    .attr('x1', currentWeekXValue)
+                    .attr('y1', 0)
+                    .attr('x2', currentWeekXValue)
+                    .attr('y2', height)
+                    .attr('stroke', 'darkorange')
+                    .attr('stroke-width', '1')
+                currentLine.append('text')
+                    .attr('text-anchor', 'middle')
+                    .attr("x", currentWeekXValue - 30)
+                    .attr("dx", '.1em')
+                    .attr("y", ".1em")
+                    .attr("dy", ".9em")
+                    .attr('stroke', 'orange')
+                    .attr("background-color", 'whitesmoke')
+                    .text('Current')
+            }
+        if(maxPossibleWeekXValue.length!=0) {
+            var maxLine = svg.append("g")
+            maxLine.append('line')
+                .attr('x1', maxPossibleWeekXValue)
+                .attr('y1', 0)
+                .attr('x2', maxPossibleWeekXValue)
+                .attr('y2', height)
+                .attr('stroke', 'green')
+                .attr('stroke-width', '1')
+            maxLine.append('text')
+                .attr('text-anchor', 'middle')
+                .attr("x", maxPossibleWeekXValue - 50)
+                .attr("dx", '.1em')
+                .attr("y", ".1em")
+                .attr("dy", ".9em")
+                .attr('stroke', 'green')
+                .text('Max Possible')
+        }
 
         // d3.select(window).on('resize', resize);
         // function resize () {
