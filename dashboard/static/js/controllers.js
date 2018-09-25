ef9142a9b488f19212befbe2e65c0998174d3d5d
Pushyami Gundala <pushyamig@users.noreply.github.com>
2018-09-25 09:04:03 -0400

    I6 assign (#122)
    * #i16,#i106-addressing the rules flow, bootstrap update
    
    * #16 weight check from DB
    
    * #16Adding tick formatting to %
    
    * #16 fixing the tool tip of the progress bar
    
    * #23, #78 assignment view alignment fixes
    
    * Codacy review changes
    
    * current week scroll fixes

--- dashboard/static/js/controllers.js
+++ dashboard/static/js/controllers.js
@@ -1,8 +1,9 @@
 visApp.controller('mainController', ['$scope', 'Get','$location', '$anchorScroll', function($scope, Get,$location,$anchorScroll) {
     $scope.data=[]
     $scope.percentFilterEnabled=true;
-    $scope.percent = ["0%","2%","5%","10%","20%","50%","75%"];
-    $scope.percent_filter=$scope.percent[0];
+    $scope.percent = [{ "value": 0, "text": "0% (all)" }, { "value": 2, "text": "2%" },{ "value": 5, "text": "5%" },
+        { "value": 10, "text": "10%" },{ "value": 20, "text": "20%" },{ "value": 50, "text": "50%" },{ "value": 75, "text": "75%" }];
+    $scope.percent_filter=$scope.percent[0].value;
     dataCall();
 
     $scope.updatePercentFilter = function() {
@@ -10,17 +11,19 @@ visApp.controller('mainController', ['$scope', 'Get','$location', '$anchorScroll
     };
 
     function dataCall(){
-        var selection = $scope.percent_filter;
-        var dataFile = '/assignment_view?percent='+selection.substring(0, selection.length - 1);
+        var dataFile = '/assignment_view?percent='+$scope.percent_filter;
         Get.getData(dataFile).then(function(data) {
             $scope.data = data.data
-            $scope.data.forEach(function (e) {
+            scrollId=1
+            $scope.data.find(function (e) {
                 current_week_indicator=e.due_date_items[0].assignment_items[0].current_week
                 if(current_week_indicator){
-                    $location.hash(e.id);
-                    $anchorScroll();
+                    return scrollId=e.id;
                 }
-            });
+
+            })
+            $location.hash(scrollId);
+            $anchorScroll();
         });
     }
 
