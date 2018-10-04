08989764e8f8f346d28d31db5481b6415907ae4c
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2018-10-03 20:04:22 -0400

    Fix django issue about is_anonymous django deprecation fixes #126 (#160)
    * Fix django issue about is_anonymous django deprecation fixes #126
    
    * Only ignore DeprecationWarning

--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
+import warnings
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dashboard.settings")
+
+    # Regex for which modules to ignore warnings from
+    IGNORE_MODULES = 'djangosaml2'
+    warnings.filterwarnings("ignore", module=IGNORE_MODULES, category=DeprecationWarning)
 
     from django.core.management import execute_from_command_line
 
