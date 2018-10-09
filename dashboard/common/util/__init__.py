d708032d1536d6f8a710b6a8981df6cc07b2ff46
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2018-10-09 17:45:38 -0400

    Some changes for issue_186 based on review comments

--- dashboard/common/util/__init__.py
+++ dashboard/common/util/__init__.py
@@ -3,16 +3,16 @@
 import django
 
 def get_course_name_from_id(course_id):
-    """ Returns the course name given the canvas ID
+    """[Get the long course name from the id]
     
-    Arguments:
-        course_id {int} -- Canvas Course ID
-    
-    Returns:
-        [str] -- Course Name
+    :param course_id: [Canvas course ID without UDW PREFIX]
+    :type course_id: [str]
+    :return: [Course Name of course or blank not found]
+    :rtype: [str]
     """
+
     course_id = str(django.conf.settings.UDW_ID_PREFIX) + str(course_id)
-    course_name = None
+    course_name = ""
     if (course_id):
         with django.db.connection.cursor() as cursor:
             cursor.execute("SELECT name FROM course WHERE id = %s", [course_id])
@@ -22,21 +22,19 @@ def get_course_name_from_id(course_id):
     return course_name
 
 def get_default_user_course_id(user_id):
-    """ Returns the current Canvas course id 
-        First return the ID from the request
-        If there is no id in the request try to find the default for the user
-        If there is no default return nothing
-    Arguments:
-        user_id {[int]} -- User Id
-    
-    Returns:
-        [str] -- Canvas Course Id
+    """[Get the default course id for the user id from the user table]
+    :param user_id: [SIS User ID of the user]
+    :type user_id: [str]
+    :return: [Canvas Course ID]
+    :rtype: [str]
     """
     course_id = ""
     with django.db.connection.cursor() as cursor:
-        cursor.execute("SELECT course_id FROM user WHERE sis_name= %s LIMIT 1", [user_id])
+        cursor.execute("SELECT course_id FROM user WHERE sis_name= %s ORDER BY course_id DESC LIMIT 1", [user_id])
         row = cursor.fetchone()
         if (row != None):
             #Remove the UDW_ID_PREFIX and just return the course_id
             course_id = str(row[0]).replace(str(django.conf.settings.UDW_ID_PREFIX),"")
     return course_id
+
+
