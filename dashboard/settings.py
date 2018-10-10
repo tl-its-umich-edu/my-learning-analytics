f30b582bf135e52797ca731176e8a1c10bedeae3
Zhen Qian <zqian@umich.edu>
2018-10-10 13:09:03 -0400

    issue_231 cron job need to refresh file table

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -306,6 +306,9 @@ else:
 # This is fixed from UDW
 UDW_ID_PREFIX = config("UDW_ID_PREFIX", default="17700000000")
 
+# This is fixed from UDW
+UDW_FILE_ID_PREFIX = config("UDW_FILE_ID_PREFIX", default="1770000000")
+
 # TODO: Select a default course for user from database, just here so things don't all break
 DEFAULT_COURSE_IDS = config("CANVAS_COURSE_IDS", default="0", cast=Csv())
 
