2d04173499c9b8462706945dc821b2f46b828253
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2019-01-17 12:27:42 -0500

    Adding wait for attach option for cron and open another port for cron

--- manage.py
+++ manage.py
@@ -9,6 +9,9 @@ if __name__ == "__main__":
         import ptvsd
         try: 
             ptvsd.enable_attach(address = (os.environ.get("PTVSD_ADDRESS",'0.0.0.0'), os.environ.get("PTVSD_PORT", 3000)))
+            # If the user wants to wait to start until it's attached call this
+            if (os.environ.get("PTVSD_WAIT_FOR_ATTACH", False)):
+                ptvsd.wait_for_attach()
         except Exception as e:
             pass
 
