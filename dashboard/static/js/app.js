54193c4b04e1e707a0b2cae2c94d3d9f5f9ca9c4
Pushyami Gundala <pushyamig@users.noreply.github.com>
2019-01-24 15:46:22 -0500

    #376 making the set default calls as PUT (#377)
    * #376 making the set default calls as PUT
    
    * #376 adding content type

--- dashboard/static/js/app.js
+++ dashboard/static/js/app.js
@@ -1,1 +1,8 @@
 var visApp = angular.module('visApp', []);
+
+// ensure csrf safe calls
+// https://docs.djangoproject.com/en/2.1/ref/csrf/#acquiring-the-token-if-csrf-use-sessions-or-csrf-cookie-httponly-is-true
+visApp.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+}]);
