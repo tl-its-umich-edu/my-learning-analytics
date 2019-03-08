cc5b6cfe533f461bc89c65c360c9def7d49e1126
Justin Lee <justinlee0022@gmail.com>
2019-03-08 15:54:20 -0800

    Assignment bug fix (#418)
    Fixed bug in horizontal bar in assignment planning table

--- assets/src/components/HorizontalBar.js
+++ assets/src/components/HorizontalBar.js
@@ -1,10 +1,8 @@
 import createHorizontalBar from './d3/createHorizontalBar'
-import withResponsiveness from './withResponsiveness'
 import createChartComponent from './createChartComponent'
 import compose from '../util/compose'
 
 const HorizontalBar = compose(
-  withResponsiveness,
   createChartComponent
 )(createHorizontalBar)
 
