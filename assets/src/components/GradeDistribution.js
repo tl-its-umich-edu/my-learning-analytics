e53cf4b13b3fd40947ab275b89b10cb94e1ff9b9
Justin Lee <justinlee0022@gmail.com>
2019-03-13 06:58:35 -0700

    Remove unused components (#431)
    * Remove unused components
    
    * Remove unused components, restored FilesAccessed

--- assets/src/components/GradeDistribution.js
+++
@@ -1,9 +0,0 @@
-import createHistogram from './d3/createHistogram'
-import withResponsiveness from './withResponsiveness'
-import createChartComponent from './createChartComponent'
-import compose from '../util/compose'
-
-export default compose(
-  withResponsiveness,
-  createChartComponent
-)(createHistogram)
