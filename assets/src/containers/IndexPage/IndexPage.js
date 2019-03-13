edfbf61e6445d5811858d95d99e9172d9e717d7d
Adonais0 <chuyao@umich.edu>
2019-03-13 19:54:56 -0400

    initial home page

---
+++ assets/src/containers/IndexPage/IndexPage.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import './IndexPage';
+
+const IndexPage = () => {
+    return (
+        <p className="test">Test</p>
+    )
+}
+
+export default IndexPage;
