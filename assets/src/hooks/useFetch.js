5cc85615a4a3adf82df7e98c8d88a911c027781d
Justin Lee <justinlee0022@gmail.com>
2019-03-13 13:59:21 -0700

    Refactored useFetch to return tuple (#435)

--- assets/src/hooks/useFetch.js
+++ assets/src/hooks/useFetch.js
@@ -5,19 +5,23 @@ const cache = new Map()
 
 const useFetch = dataURL => {
   const [data, setData] = useState(null)
+  const [loaded, setLoaded] = useState(false)
 
   useEffect(() => {
     if (cache.has(dataURL)) {
       setData(cache.get(dataURL))
+      setLoaded(true)
     } else {
+      setLoaded(false)
       get(dataURL).then(data => {
         cache.set(dataURL, data)
         setData(data)
+        setLoaded(true)
       })
     }
   }, [dataURL])
 
-  return data
+  return [loaded, data]
 }
 
 export default useFetch
