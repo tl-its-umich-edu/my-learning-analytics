0fac8286db76fbd0e46971cbbead68be7a2000c6
Justin Lee <justinlee0022@gmail.com>
2019-03-13 08:21:48 -0700

    Added snapshot testing for d3 bar chart

--- assets/src/__tests__/util/math.test.js
+++ assets/src/__tests__/util/math.test.js
@@ -1,7 +1,7 @@
-import math from '../../util/math'
+import { average } from '../../util/math'
 
 describe('average', () => {
   it('takes an array of numbers and calculates average', () => {
-    expect(math.average([1,2,3,4,5])).toEqual(3)
+    expect(average([1,2,3,4,5])).toEqual(3)
   })
 })
