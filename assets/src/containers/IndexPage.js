d4b301fe2883bd42e5c25a32479215e9ea1b6727
Adonais0 <chuyao@umich.edu>
2019-04-02 14:01:13 -0400

    Fix index display bug

--- assets/src/containers/IndexPage.js
+++ assets/src/containers/IndexPage.js
@@ -9,6 +9,10 @@ const styles = theme => ({
     root: {
         flexGrow: 1,
       },
+    container: {
+        display: "flex",
+        justifyContent: "center"
+    },
     wrapper: {
         maxWidth: 1023,
         margin: theme.spacing.unit * 2 + "px auto",
@@ -22,7 +26,7 @@ function IndexPage (props) {
     const currentCourseId = match.params.courseId
     return (
         <Grid container spacing={16} className={classes.root}>
-            <Grid item xs={12}>
+            <Grid item xs={12} className={classes.container}>
                 <Grid 
                 container 
                 className={classes.wrapper} 
