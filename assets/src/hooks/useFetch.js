34ba19707a1b28d373c15e2b7d0f08a9868c7531
Justin Lee <justinlee0022@gmail.com>
2019-04-03 10:28:36 -0700

    Expanded useFetch to support 'put', 'post', etc. (#476)

--- assets/src/hooks/useFetch.js
+++ assets/src/hooks/useFetch.js
@@ -2,33 +2,46 @@
 import { useState, useEffect } from 'react'
 
 const cache = new Map()
-// https://sudo.isl.co/fetch-me-that-json-from-django/
-const get = url => {
-  return fetch(url,{
-    headers: {
-      'Accept': 'application/json',
-      'X-Requested-With': 'XMLHttpRequest'
-    },
-    credentials: 'include'
-  })
-    .then(res => res.json())
+
+const defaultFetchOptions = {
+  headers: {
+    'Accept': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest'
+  },
+  credentials: 'include'
 }
 
-const useFetch = dataURL => {
+const useFetch = (dataURL, options) => {
+  const fetchOptions = options
+    ? { ...options, ...defaultFetchOptions }
+    : { method: 'get', ...defaultFetchOptions }
+
   const [data, setData] = useState(null)
   const [loaded, setLoaded] = useState(false)
 
   useEffect(() => {
-    if (cache.has(dataURL)) {
-      setData(cache.get(dataURL))
-      setLoaded(true)
-    } else {
-      setLoaded(false)
-      get(dataURL).then(data => {
-        cache.set(dataURL, data)
-        setData(data)
+    if (fetchOptions.method === 'get') {
+      // only cache 'get' calls
+      if (cache.has(dataURL)) {
+        setData(cache.get(dataURL))
         setLoaded(true)
-      })
+      } else {
+        setLoaded(false)
+        fetch(dataURL, fetchOptions)
+          .then(res => res.json())
+          .then(data => {
+            cache.set(dataURL, data)
+            setData(data)
+            setLoaded(true)
+          })
+      }
+    } else {
+      fetch(dataURL, fetchOptions)
+        .then(res => res.json())
+        .then(data => {
+          setData(data)
+          setLoaded(true)
+        })
     }
   }, [dataURL])
 
