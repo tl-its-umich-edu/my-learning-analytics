6147a1322ca6221ccc43f4d74979b9d716a2ab3a
Justin Lee <justinlee0022@gmail.com>
2019-05-01 08:19:08 -0700

    Added equivalent hooks to HOCs (#499)
    * Refactored el -> domElement and setEl -> setDomElement

--- assets/src/components/Histogram.js
+++ assets/src/components/Histogram.js
@@ -1,11 +1,17 @@
+import React, { useState } from 'react'
 import createHistogram from './d3/createHistogram'
-import withResponsiveness from './withResponsiveness'
-import createChartComponent from './createChartComponent'
-import compose from '../util/compose'
+import useCreateChart from '../hooks/useCreateChart'
+import useResponsiveness from '../hooks/useResponsiveness'
 
-const Histogram = compose(
-  withResponsiveness,
-  createChartComponent
-)(createHistogram)
+function Histogram (props) {
+  const [domElement, setDomElement] = useState(null)
+
+  const [width, height] = useResponsiveness({ ...props, domElement })
+  useCreateChart({ ...props, domElement, width, height }, createHistogram)
+
+  return (
+    <div ref={domElement => setDomElement(domElement)} />
+  )
+}
 
 export default Histogram
