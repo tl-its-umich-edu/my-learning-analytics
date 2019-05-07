4c5ab11cc412310398d7fe7164bc91c6aec98b5c
Justin Lee <justinlee0022@gmail.com>
2019-05-07 14:25:06 -0700

    Removed more unused components, updated tests,

--- assets/src/__tests__/d3/createHistogram.test.js
+++ assets/src/__tests__/d3/createHistogram.test.js
@@ -1,21 +1,34 @@
 /* global describe, test, expect */
 import createHistogram from '../../components/d3/createHistogram'
-import {histogramData, histogramDataMoreThan100Percent} from '../testData/d3TestData'
+import { histogramData, histogramDataMoreThan100Percent } from '../testData/d3TestData'
 
 describe('createHistogram', () => {
   test('should build a histogram', () => {
     const div = document.createElement('div')
-    createHistogram({ data: histogramData, width: 1000, height: 500, el: div })
+    createHistogram({ data: histogramData, width: 1000, height: 500, domElement: div })
     expect(div).toMatchSnapshot()
   })
   test('should build a histogram bar chart with MyGrade line', () => {
     const div = document.createElement('div')
-    createHistogram({ data: histogramData, width: 1000, height: 500,  el: div, myGrade:79.8})
+    createHistogram({
+      data: histogramData,
+      width: 1000,
+      height: 500,
+      domElement: div,
+      myGrade: 79.8
+    })
     expect(div).toMatchSnapshot()
   })
   test('should build a histogram bar chart with MyGrade line and distribution more than 100', () => {
     const div = document.createElement('div')
-    createHistogram({ data: histogramDataMoreThan100Percent, width: 1000, height: 500,  el: div, myGrade:56.8, maxGrade: 110})
+    createHistogram({
+      data: histogramDataMoreThan100Percent,
+      width: 1000,
+      height: 500,
+      domElement: div,
+      myGrade: 56.8,
+      maxGrade: 110
+    })
     expect(div).toMatchSnapshot()
   })
 })
