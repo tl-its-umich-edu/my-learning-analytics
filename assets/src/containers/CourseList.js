706eba380e542b421f2d223780b30d7c7d33b37f
Justin Lee <justinlee0022@gmail.com>
2019-06-12 13:20:19 -0700

    Added app bar with avatar after login

--- assets/src/containers/CourseList.js
+++ assets/src/containers/CourseList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import SelectCard from '../components/SelectCard'
@@ -6,6 +6,13 @@ import Error from './Error'
 import Typography from '@material-ui/core/Typography'
 import { Link } from 'react-router-dom'
 import Paper from '@material-ui/core/Paper'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import Avatar from '@material-ui/core/Avatar'
+import Popover from '@material-ui/core/Popover'
+import AvatarModal from '../components/AvatarModal'
 
 const styles = theme => ({
   root: {
@@ -24,6 +31,9 @@ const styles = theme => ({
   paper: {
     padding: theme.spacing.unit * 2,
     color: theme.palette.text.secondary
+  },
+  grow: {
+    flexGrow: 1
   }
 })
 
@@ -31,22 +41,64 @@ function CourseList (props) {
   const { classes } = props
   if (!myla_globals.username) return (window.location.href = myla_globals.login)
   if (myla_globals.is_superuser) return (<Paper className={classes.paper}><Typography>Select a course you choose to look in</Typography></Paper>)
-  let userCourseInfo = ''
-  if (myla_globals.user_courses_info.length !== 0) {
-    userCourseInfo = JSON.parse(myla_globals.user_courses_info)
-  }
+
+  const userCourseInfo = (myla_globals.user_courses_info.length !== 0)
+    ? JSON.parse(myla_globals.user_courses_info)
+    : ''
+
   if (!userCourseInfo) return (<Error>You are not enrolled in any courses with MyLA enabled.</Error>)
 
+  const user = {
+    username: myla_globals.username,
+    admin: myla_globals.is_superuser
+  }
+
+  const [avatarEl, setAvatarEl] = useState(null)
+  const avatarOpen = Boolean(avatarEl)
+
   return (
-    <Grid container spacing={16} className={classes.root}>
-      <Grid item xs={12} className={classes.container}>
-        {userCourseInfo.map((course, key) =>
-          <Link style={{ textDecoration: 'none' }} to={course.course_id} key={key}>
-            <SelectCard cardData={{ title: course.course_name }} />
-          </Link>
-        )}
+    <>
+      <AppBar className={classes.root} position='static'>
+        <Toolbar>
+          <Typography variant='h6' color='inherit' className={classes.grow}>
+            My Learning Analytics
+          </Typography>
+          <div className={classes.grow} />
+          <IconButton
+            aria-owns={avatarOpen ? 'simple-popper' : undefined}
+            onClick={event => setAvatarEl(event.currentTarget)}
+            color='inherit'
+            aria-haspopup='true'
+            variant='contained'>
+            <Avatar>{user.username.slice(0, 1)}</Avatar>
+          </IconButton>
+          <Popover
+            open={avatarOpen}
+            anchorEl={avatarEl}
+            onClose={() => setAvatarEl(null)}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center'
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center'
+            }}
+          >
+            <AvatarModal user={user} />
+          </Popover>
+        </Toolbar>
+      </AppBar>
+      <Grid container spacing={16} className={classes.root}>
+        <Grid item xs={12} className={classes.container}>
+          {userCourseInfo.map((course, key) =>
+            <Link style={{ textDecoration: 'none' }} to={course.course_id} key={key}>
+              <SelectCard cardData={{ title: course.course_name }} />
+            </Link>
+          )}
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   )
 }
 
