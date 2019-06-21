127fa8bfd84ecf1251556305d74a6e07915f8d20
Andrew Gardener <andrew.e.gardener@gmail.com>
2019-06-21 09:47:47 -0700

    Add google analytics to react (#539)
    - GoogleAnalyticsTracking component will track page views when initialized and every time the route is changed
    - refactored how routers/routers are setup so there is only one router in the application (app is now the main routing entry point which can have components added like CourseList and the new Course)
    - Note: static standalone pages are not currently tracked (ex: login page which will eventually be converted to a react page)

---
+++ assets/src/components/GoogleAnalyticsTracking.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react"
+import GoogleAnalytics from "react-ga"
+import { withRouter } from 'react-router-dom'
+
+function GoogleAnalyticsTracking (props) {
+  const {
+    gaId
+  } = props
+
+  const [initialized, setInitialized] = useState(false)
+  const [previousPage, setPreviousPage] = useState(null)
+
+  if (gaId && !initialized) {
+    setInitialized(true)
+    GoogleAnalytics.initialize(gaId)
+  }
+
+  useEffect(() => {
+    const page = window.location.pathname + window.location.search + window.location.hash
+    if (gaId && page != previousPage) {
+      setPreviousPage(page)
+      GoogleAnalytics.pageview(page)
+    }
+  })
+
+  return null
+}
+
+export default withRouter(GoogleAnalyticsTracking)
