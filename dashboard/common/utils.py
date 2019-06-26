f5a4441cc49027c43fdf9afbcc30e4bbf37ffed8
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2019-06-26 09:48:10 -0400

    Fixes from code review

--- dashboard/common/utils.py
+++ dashboard/common/utils.py
@@ -1,13 +1,12 @@
-import logging
+import logging, os
 logger = logging.getLogger(__name__)
-from django.conf import settings
 
 def get_build_info():
     logger.debug(get_build_info.__name__)
-    git_commit=settings.ENV.get("OPENSHIFT_BUILD_COMMIT", "")
-    build_namespace=settings.ENV.get("OPENSHIFT_BUILD_NAMESPACE", "")
-    git_branch=settings.ENV.get("OPENSHIFT_BUILD_REFERENCE", "master")
-    build_source=settings.ENV.get("OPENSHIFT_BUILD_SOURCE", "")
-    build_name=settings.ENV.get("OPENSHIFT_BUILD_NAME", "")
+    git_commit=os.getenv("OPENSHIFT_BUILD_COMMIT", "")
+    build_namespace=os.getenv("OPENSHIFT_BUILD_NAMESPACE", "")
+    git_branch=os.getenv("OPENSHIFT_BUILD_REFERENCE", "master")
+    build_source=os.getenv("OPENSHIFT_BUILD_SOURCE", "")
+    build_name=os.getenv("OPENSHIFT_BUILD_NAME", "")
     return f'Build_Namespace:{build_namespace} Build_Name: {build_name} Git_Source: {build_source} Git_Branch: {git_branch} Git_Commit: {git_commit}'
 
