85c65f459a3784177f153ff23e3d68e28331a710
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2019-06-26 14:24:20 -0400

    Fixing env_to_json for booleans, fixing sample for booleans

--- env_to_json.py
+++ env_to_json.py
@@ -4,6 +4,19 @@ import json
 import sys
 import csv
 
+_BOOLEANS = {'1': True, 'yes': True, 'true': True, 'on': True,
+             '0': False, 'no': False, 'false': False, 'off': False}
+
+def cast_boolean(value):
+    """
+    Helper to convert config values to boolean as ConfigParser do.
+    """
+    value = str(value)
+    if value.lower() not in _BOOLEANS:
+        return value
+
+    return _BOOLEANS[value.lower()]
+
 try:
     dotenv = sys.argv[1]
 except IndexError as e:
@@ -27,12 +40,12 @@ for x in content:
            y[1] = list(csv.reader([y[1]]))[0]
         else:
            y[1] = y[1].strip() 
-
-        # Try to convert to integer
-        try:
-            y[1] = int(y[1])
-        except:
-            pass
+           # Try to convert to integer
+           try:
+               y[1] = int(y[1])
+           except:
+               # Try to convert to boolean
+               y[1] = cast_boolean(y[1])
         newcontent.append(y)
 
 #print('My list:', *newcontent, sep='\n- ')
