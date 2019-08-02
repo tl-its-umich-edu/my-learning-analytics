9dd46c141dfd3a83259e0ffeb4f13d2f192b9dd6
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2019-08-02 16:18:38 -0400

    Applying same fix to assignment planning view

--- assets/src/util/object.js
+++ assets/src/util/object.js
@@ -1,5 +1,4 @@
-const isObjectEmpty = obj => obj.constructor === Object && Object.entries(obj).length === 0
-
+const isObjectEmpty = obj => Object.keys(obj).length === 0
 
 // could use Object.values but support isn't widespread yet
 const getObjectValues = obj => Object.keys(obj).map(key => obj[key])
