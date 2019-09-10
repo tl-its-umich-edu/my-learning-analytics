ae950f4b3036777e397805f628f0c6933d3497e2
Sam Sciolla <ssciolla@umich.edu>
2019-09-10 18:07:31 -0400

    Updating course, term commands; changing demo_init.sh to sample

--- dashboard/management/commands/course.py
+++ dashboard/management/commands/course.py
@@ -1,26 +1,38 @@
 from django.core.management.base import BaseCommand
 from dashboard.models import Course, CourseViewOption, AcademicTerms
 from dashboard.common.db_util import canvas_id_to_incremented_id
+from datetime import datetime
+import pytz
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--course_id', dest='course_id', type=int, required=True)
-        parser.add_argument('--term_id', dest='term_id', type=int, required=True)
+        parser.add_argument('--term_id', dest='term_id', type=int, required=False)
         parser.add_argument('--name', dest='name', type=str, required=True)
+        parser.add_argument('--date_start', dest='date_start', type=str, required=False)
+        parser.add_argument('--date_end', dest='date_end', type=str, required=False)
 
     def handle(self, *args, **options):
         course_id = options.get('course_id')
         term_id = options.get('term_id')
         name = options.get('name')
+        date_start = options.get('date_start')
+        if date_start is not None:
+            date_start = datetime.strptime(date_start, '%Y-%m-%d %H:%M:%S').replace(tzinfo=pytz.UTC)
+        date_end = options.get('date_end')
+        if date_end is not None:
+            date_end = datetime.strptime(date_end, '%Y-%m-%d %H:%M:%S').replace(tzinfo=pytz.UTC)
 
         prefixed_course_id = canvas_id_to_incremented_id(course_id)
-        prefixed_term_id = canvas_id_to_incremented_id(term_id)
-
-        try:
-            term_obj = AcademicTerms.objects.get(id=prefixed_term_id)
-        except AcademicTerms.DoesNotExist:
-            self.stdout.write (f"Error: Term {term_id} does not exists.")
-            return
+        if term_id is not None:
+            prefixed_term_id = canvas_id_to_incremented_id(term_id)
+            try:
+                term_obj = AcademicTerms.objects.get(id=prefixed_term_id)
+            except AcademicTerms.DoesNotExist:
+                self.stdout.write (f"Error: Term {term_id} does not exists.")
+                return
+        else:
+            term_obj = None
 
         is_new = False
         try:
@@ -33,7 +45,9 @@ class Command(BaseCommand):
 
         course_obj.canvas_id = course_id
         course_obj.name = name
-        course_obj.term_id = term_obj
+        course_obj.term = term_obj
+        course_obj.date_start = date_start
+        course_obj.date_end = date_end
         course_obj.save()
 
         if is_new:
