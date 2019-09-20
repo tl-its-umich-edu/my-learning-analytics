d6b16f1b4249418bfa38f2098f46d76097bf6c0c
Vincent Cao <33735083+vjcao@users.noreply.github.com>
2019-09-20 15:55:32 -0400

    Issue 687 (#699)

--- dashboard/rules.py
+++ dashboard/rules.py
@@ -35,7 +35,6 @@ rules.add_perm('dashboard.grade_distribution', is_admin_or_enrolled_in_course)
 rules.add_perm('dashboard.update_user_default_selection_for_views', is_admin_or_enrolled_in_course)
 rules.add_perm('dashboard.get_user_default_selection', is_admin_or_enrolled_in_course)
 rules.add_perm('dashboard.assignments', is_admin_or_enrolled_in_course)
-rules.add_perm('dashboard.courses_enabled', is_admin)
 
 # templates
 rules.add_perm('dashboard.get_course_template', is_admin_or_enrolled_in_course)
