abd78f18a1fb092b8390aa43d11cd0cb48422559
Sam Sciolla <ssciolla@umich.edu>
2019-09-23 17:41:59 -0400

    First run at displaying build info

--- dashboard/common/utils.py
+++ dashboard/common/utils.py
@@ -4,12 +4,22 @@ logger = logging.getLogger(__name__)
 
 def get_build_info():
     logger.debug(get_build_info.__name__)
-    git_commit=os.getenv("OPENSHIFT_BUILD_COMMIT", "")
-    build_namespace=os.getenv("OPENSHIFT_BUILD_NAMESPACE", "")
-    git_branch=os.getenv("OPENSHIFT_BUILD_REFERENCE", "master")
-    build_source=os.getenv("OPENSHIFT_BUILD_SOURCE", "")
-    build_name=os.getenv("OPENSHIFT_BUILD_NAME", "")
-    return f'Build_Namespace:{build_namespace} Build_Name: {build_name} Git_Source: {build_source} Git_Branch: {git_branch} Git_Commit: {git_commit}'
+
+    commit = os.getenv("OPENSHIFT_BUILD_COMMIT", ""),
+    if commit != "" and len(commit) > 7:
+        commit_abbrev = commit[:7]
+    else:
+        commit_abbrev = ""
+
+    build = {
+        "build_namespace": os.getenv("OPENSHIFT_BUILD_NAMESPACE", ""),
+        "build_name": os.getenv("OPENSHIFT_BUILD_NAME", ""),
+        "git_source": os.getenv("OPENSHIFT_BUILD_SOURCE", ""),
+        "git_branch": os.getenv("OPENSHIFT_BUILD_REFERENCE", "master"),
+        "git_commit": commit,
+        "git_commit_abbrev": commit_abbrev
+    }
+    return build
 
 
 def look_up_key_for_value(myDict, searchFor):
