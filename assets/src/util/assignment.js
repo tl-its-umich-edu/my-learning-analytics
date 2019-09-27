e2d1c79130b2637625557dedf751e7782c228c13
Justin Lee <justin.lee@ubc.ca>
2019-09-27 16:04:34 -0700

    Refactored calculateMaxPossibleCourseGrade

--- assets/src/util/assignment.js
+++ assets/src/util/assignment.js
@@ -6,25 +6,26 @@ const calculateAssignmentGoalsFromCourseGoal = (assignments, courseGoalGrade) =>
 
 const calculateWeightOfAssignment = (pointsPossible, assignmentGroupId, assignmentGroups) => {
   const assignmentGroup = assignmentGroups.find(aGroup => aGroup.id === assignmentGroupId)
-  const assignmentGrade = assignmentGroup.weight * (pointsPossible / assignmentGroup.groupPoints)
-  return roundToOneDecimal(assignmentGrade)
+  const assignmentWeight = assignmentGroup.weight * (pointsPossible / assignmentGroup.groupPoints)
+  return roundToOneDecimal(assignmentWeight)
 }
 
 const calculateMaxPossibleCourseGrade = (assignments, assignmentGroups) => {
   const [totalUserPoints, totalPossiblePoints] = assignments
     .filter(assignment => assignment.currentUserSubmission)
     .reduce((acc, assignment) => {
-      const assignmentGroup = assignmentGroups.find(aGroup => aGroup.id === assignment.assignmentGroupId)
       const assignmentGrade = assignment.currentUserSubmission.score / assignment.pointsPossible
-      const totalPointsPossible = assignmentGroup.weight * (assignment.pointsPossible / assignmentGroup.groupPoints)
-      const pointsTowardsFinalGrade = assignmentGrade * totalPointsPossible
+      const weightOfAssignment = calculateWeightOfAssignment(assignment.pointsPossible, assignment.assignmentGroupId, assignmentGroups)
+      const pointsTowardsFinalGrade = assignmentGrade * weightOfAssignment
 
       acc[0] += pointsTowardsFinalGrade
-      acc[1] += totalPointsPossible
+      acc[1] += weightOfAssignment
       return acc
     }, [0, 0])
   return roundToOneDecimal(totalUserPoints / totalPossiblePoints * 100)
 }
+
+// const calculateTotalCourseGrade
 
 export {
   calculateAssignmentGoalsFromCourseGoal,
