5f10845495198f118a6eb7e5bfd2e0c5ce088bf4
Sam Sciolla <ssciolla@umich.edu>
2019-10-14 11:19:55 -0400

    Move env var setting to if else block; add values filter

--- start.sh
+++ start.sh
@@ -27,6 +27,8 @@ else
     GUNICORN_RELOAD=""
 fi
 
+DOMAIN_JQ='.ALLOWED_HOSTS | . - ["127.0.0.1", "localhost", ".ngrok.io"] | if . | length == 0 then "localhost" else .[0] end'
+
 if [ -z "${ENV_JSON}" ]; then
     MYSQL_HOST=$(jq -r -c ".MYSQL_HOST | values" ${ENV_FILE})
     MYSQL_PORT=$(jq -r -c ".MYSQL_PORT | values" ${ENV_FILE})
@@ -34,6 +36,8 @@ if [ -z "${ENV_JSON}" ]; then
     PTVSD_ENABLE=$(jq -r -c ".PTVSD_ENABLE | values" ${ENV_FILE})
     CRONTAB_SCHEDULE=$(jq -r -c ".CRONTAB_SCHEDULE | values" ${ENV_FILE})
     RUN_AT_TIMES=$(jq -r -c ".RUN_AT_TIMES | values" ${ENV_FILE})
+    DOMAIN=$(jq -r -c "${DOMAIN_JQ} | values" ${ENV_FILE})
+    PORT=$(jq -r -c ".LOCALHOST_PORT | values" ${ENV_FILE})
 else
     MYSQL_HOST=$(echo "${ENV_JSON}" | jq -r -c ".MYSQL_HOST | values")
     MYSQL_PORT=$(echo "${ENV_JSON}" | jq -r -c ".MYSQL_PORT | values")
@@ -41,6 +45,8 @@ else
     PTVSD_ENABLE=$(echo "${ENV_JSON}" | jq -r -c ".PTVSD_ENABLE | values")
     CRONTAB_SCHEDULE=$(echo "${ENV_JSON}" | jq -r -c ".CRONTAB_SCHEDULE | values")
     RUN_AT_TIMES=$(echo "${ENV_JSON}" | jq -r -c ".RUN_AT_TIMES | values")
+    DOMAIN=$(echo "${ENV_JSON}" | jq -r -c "${DOMAIN_JQ} | values")
+    PORT=$(echo "${ENV_JSON}" | jq -r -c ".LOCALHOST_PORT | values")
 fi
 
 echo "Waiting for DB"
@@ -57,9 +63,6 @@ echo Running python startups
 python manage.py migrate
 
 echo "Setting domain of default site record"
-DOMAIN_JQ='.ALLOWED_HOSTS | . - ["127.0.0.1", "localhost", ".ngrok.io"] | if . | length == 0 then "localhost" else .[0] end'
-DOMAIN=$(jq -r -c "${DOMAIN_JQ}" ${ENV_FILE})
-PORT="$(jq -r -c ".LOCALHOST_PORT" ${ENV_FILE})"
 if [ ${DOMAIN} == "localhost" ]; then
   python manage.py site --domain="${DOMAIN}:${PORT}" --name="${DOMAIN}"
 else
