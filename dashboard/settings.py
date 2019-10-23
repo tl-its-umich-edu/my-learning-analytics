1eb6b936276d5adc5620c9954f2bb3f9d6e6ba5d
Sam Sciolla <ssciolla@umich.edu>
2019-10-23 16:55:37 -0400

    Remove SessionAuthenticationMiddleware

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -115,7 +115,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
