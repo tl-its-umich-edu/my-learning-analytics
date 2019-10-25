65b4d32230d1cba871570f0d5915558891d1e8b4
vjcao <vjcao@umich.edu>
2019-10-25 13:54:08 -0400

    addressing codacy bot

--- dashboard/views.py
+++ dashboard/views.py
@@ -37,10 +37,10 @@ NO_GRADE_STRING = "NO_GRADE"
 # string for resource type
 RESOURCE_TYPE_STRING = "resource_type"
 RESOURCE_VALUES = settings.RESOURCE_VALUES
-inverse = {}
+INVERSE = {}
 for key, value in RESOURCE_VALUES.items():
     for resource_type in value['types']:
-        inverse[resource_type] = key
+        INVERSE[resource_type] = key
 
 
 # Is courses_enabled api enabled/disabled?
@@ -294,7 +294,7 @@ def resource_access_within_week(request, course_id=0):
 
     output_df['resource_id_part'], output_df['resource_name_part'] = output_df['resource_id_name'].str.split(';', 1).str
 
-    output_df['resource_name'] = output_df.apply(lambda row: RESOURCE_URLS[row.resource_type]["prefix"] + row.resource_id_part + RESOURCE_URLS[row.resource_type]["postfix"] + CANVAS_FILE_ID_NAME_SEPARATOR + row.resource_name_part + CANVAS_FILE_ID_NAME_SEPARATOR + RESOURCE_VALUES[inverse[row.resource_type]]['icon'], axis=1)
+    output_df['resource_name'] = output_df.apply(lambda row: RESOURCE_URLS[row.resource_type]["prefix"] + row.resource_id_part + RESOURCE_URLS[row.resource_type]["postfix"] + CANVAS_FILE_ID_NAME_SEPARATOR + row.resource_name_part + CANVAS_FILE_ID_NAME_SEPARATOR + RESOURCE_VALUES[INVERSE[row.resource_type]]['icon'], axis=1)
     output_df.drop(columns=['resource_id_part', 'resource_name_part', 'resource_id_name'], inplace=True)
 
     logger.debug(output_df.to_json(orient='records'))
