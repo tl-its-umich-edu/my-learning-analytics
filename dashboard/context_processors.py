f27698457b0085bcbecd9d9b1f355d8b41ac141d
Sam Sciolla <ssciolla@umich.edu>
2019-11-07 12:48:12 -0500

    Remove unused context processor

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -5,12 +5,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def current_user_courses_info(request):
-    logger.info(current_user_courses_info.__name__)
-    courses_info_by_user = db_util.get_user_courses_info(request.user.username)
-    return {'current_user_courses_info': courses_info_by_user}
-
-
 def last_updated(request):
     return {'last_updated': db_util.get_canvas_data_date()}
 
