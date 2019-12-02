727cfd7b8727a2f4eb40628c7df684cdb4edba6c
Jianpeng Yan <jpyan@umich.edu>
2019-12-02 01:13:46 -0500

    add slide in time and change duration based on the length of message

--- assets/src/components/UserSettingSnackbar.js
+++ assets/src/components/UserSettingSnackbar.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react'
 import Snackbar from '@material-ui/core/Snackbar'
 import IconButton from '@material-ui/core/IconButton'
 import CloseIcon from '@material-ui/icons/Close'
+import Fade from '@material-ui/core/Fade';
+import Slide from '@material-ui/core/Slide';
+
+function SlideTransition(props) {
+  return <Slide {...props} direction="up" />;
+}
 
 function UserSettingSnackbar (props) {
   const {
@@ -32,7 +38,11 @@ function UserSettingSnackbar (props) {
         horizontal: 'left'
       }}
       open={savedSnackbarOpen}
-      autoHideDuration={3000}
+      // show for 50ms for each of the characters in the message
+      autoHideDuration={"{snackbarMessage}".length * 50}
+      // Fade in (or slide in) for 200-400ms for saccade time for the eye to refocus
+      TransitionComponent={SlideTransition}
+      transitionDuration={{exit: 400, enter: 800}}
       onClose={() => setSavedSnackbarOpen(false)}
       message={<span>{snackbarMessage}</span>}
       action={[
