6f8f60d63583073cc9e0c0edfacb12fa7091519d
Sam Sciolla <ssciolla@umich.edu>
2020-01-13 11:43:13 -0500

    Expose primary UI color through myla_globals

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -231,6 +231,9 @@ STATICFILES_FINDERS = (
     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
+# The hex value to be used in the frontend for the "primary" color in the theme.
+PRIMARY_UI_COLOR = ENV.get("PRIMARY_UI_COLOR", None)
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
