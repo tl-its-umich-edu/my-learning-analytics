4f34f93cb7f38c483f632d3cc526567cb0e7e701
Sam Sciolla <ssciolla@umich.edu>
2020-01-13 11:45:32 -0500

    Change siteTheme module to defaultPalette

---
+++ assets/src/defaultPalette.js
@@ -0,0 +1,19 @@
+let defaultPalette = {
+  /* The 'primary' color is used for structural components like the AppBar and SelectCard.
+     Different institutions using this application are encouraged to modify the provided value. */
+  primary: {
+    main: '#40658F'
+  },
+  /* The 'secondary' color is used currently to indicate a positive, completed, viewed, or selected state.
+     This value was arrived at through research and design work; use caution when modifying. */
+  secondary: {
+    main: '#4682B4'
+  },
+  /* The 'negative' color is used currently to indicate a negative, un-completed, un-viewed, or un-selected state.
+     This value was arrived at through research and design work; use caution when modifying. */
+  negative: {
+    main: '#E1E1E1'
+  }
+}
+
+export default defaultPalette
