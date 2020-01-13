fc4975c5374e1bf0d68ab214a3bfbcdb3796f887
Justin Lee <justin.lee@ubc.ca>
2020-01-13 13:37:56 -0800

    Merge branch 'master' into redesign-assignment

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -5,9 +5,13 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def last_updated(request):
-    return {'last_updated': db_util.get_canvas_data_date()}
-
-
 def get_git_version_info(request):
     return {'git_version': utils.get_git_version_info()}
+
+
+def get_myla_globals(request):
+    return {'myla_globals': utils.get_myla_globals(request.user)}
+
+
+def last_updated(request):
+    return {'last_updated': db_util.get_canvas_data_date()}
