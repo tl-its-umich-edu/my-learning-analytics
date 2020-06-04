9593779132012185e9b2b46ebbddd90bf9392427
Sam Sciolla <ssciolla@umich.edu>
2020-06-04 12:31:58 -0400

    Create useTimeout; add timeout to focus; remove tabIndex after header has been announced

--- assets/src/components/ViewHeader.js
+++ assets/src/components/ViewHeader.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import Typography from '@material-ui/core/Typography'
+import useTimeout from '../hooks/useTimeout'
 
 function ViewHeader (props) {
   const [domElement, setDomElement] = useState(null)
+  const [focused, setFocused] = useState(false)
+  const [announced, setAnnounced] = useState(false)
   const { children } = props
 
   // Focuses the page header only when the domElement changes.
@@ -10,16 +13,31 @@ function ViewHeader (props) {
   useEffect(() => {
     if (domElement !== null) {
       domElement.focus()
+      setFocused(true)
     }
   }, [domElement])
 
+  // Un-focuses the page header after two seconds.
+  useTimeout(() => {
+    if (focused) {
+      domElement.blur()
+      setFocused(false)
+      setAnnounced(true)
+    }
+  }, 2000)
+
+  const headerProps = {
+    variant: 'h5',
+    gutterBottom: true,
+    ref: domElement => setDomElement(domElement)
+  }
+  // Let the header be focus-able only until it has finished being announced
+  if (!announced) {
+    headerProps.tabIndex = -1
+  }
+
   return (
-    <Typography
-      variant='h5'
-      gutterBottom
-      tabIndex={-1}
-      ref={domElement => setDomElement(domElement)}
-    >
+    <Typography {...headerProps}>
       {children}
     </Typography>
   )
