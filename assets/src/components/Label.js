68dcc481a38ed613c491a11dbeae1da0303a8b1c
Chris Rowland <rowc@umich.edu>
2020-06-09 15:49:53 -0400

    removed unnecessary assignment

--- assets/src/components/Label.js
+++ assets/src/components/Label.js
@@ -3,13 +3,11 @@ import React from 'react'
 const alignThreshold = 25
 
 const getLeft = (value) => {
-  const left = value > alignThreshold ? '0' : value + '%'
-  return left
+  return value > alignThreshold ? '0' : value + '%'
 }
 
 const getRight = (value) => {
-  const right = value > alignThreshold ? value + '%' : ''
-  return right
+  return value > alignThreshold ? value + '%' : ''
 }
 
 const getWidth = (value) => {
