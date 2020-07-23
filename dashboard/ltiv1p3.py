79bf095a8a5c4f27432d435b94bb8438ce865408
pushyami <pushyami@umich.edu>
2020-07-23 13:09:15 -0400

    Error checking when LTI custom params not set

--- dashboard/ltiv1p3.py
+++ dashboard/ltiv1p3.py
@@ -1,4 +1,4 @@
-import logging, string, random, json
+import logging, string, random
 
 import django.contrib.auth
 
@@ -53,34 +53,34 @@ def get_jwks(request):
 
 
 def check_if_instructor(roles, username, course_id):
-    teaching_roles = [role for role in roles if "membership#Instructor" in role or
-                      "membership/Instructor#TeachingAssistant" in role]
+    teaching_roles = [role for role in roles if 'membership#Instructor' in role or
+                      'membership/Instructor#TeachingAssistant' in role]
     if len(teaching_roles) == 0:
         logger.info(f"user {username} is not instructor in course {course_id}")
         return False
 
-    ta = [teach_role for teach_role in teaching_roles if "membership/Instructor#TeachingAssistant" in teach_role]
+    ta = [teach_role for teach_role in teaching_roles if 'membership/Instructor#TeachingAssistant' in teach_role]
     if len(ta) != 0:
-        logger.info(f"user {username} is not a instructor of course {course_id}")
+        logger.info(f'user {username} is not a instructor of course {course_id}')
         return False
     else:
-        logger.info(f"user {username} is instructor in course {course_id}")
+        logger.info(f'user {username} is instructor in course {course_id}')
         return True
 
 
 def role_check(roles):
-    course_roles = [role for role in roles if "membership" in role]
+    course_roles = [role for role in roles if 'membership' in role]
     logger.info(course_roles)
     user_roles = []
     if len(course_roles) == 0:
         institute_roles = [role for role in roles if "institution" in role]
-        logger.info(f"user don't seems to have role in Course must be admin {institute_roles}")
+        logger.info(f'user dont seems to have role in Course must be admin {institute_roles}')
         return user_roles
 
     for role in course_roles:
         split_ = role.split('#')
         user_roles.append(split_[1])
-    logger.info(f"User role in course {user_roles}")
+    logger.info(f'User role in course {user_roles}')
     # this step eliminates duplicates of multiple same roles in the course Like TA can be instructor in multiple sections
     user_roles = list(dict.fromkeys(user_roles))
     # A TA can be instructor in other sections of the course but not in Lecture course so default to single role
@@ -92,6 +92,10 @@ def extracting_launch_variables_for_tool_use(request, message_launch):
     launch_id = message_launch.get_launch_id()
     launch_data = message_launch.get_launch_data()
     custom_params = launch_data['https://purl.imsglobal.org/spec/lti/claim/custom']
+    if not custom_params:
+        raise Exception(
+            f'Custom param LTI configuration [user_username=$User.username,canvas_course_id=$Canvas.course.id] not set'
+        )
     course_name = launch_data['https://purl.imsglobal.org/spec/lti/claim/context']['title']
     roles = launch_data['https://purl.imsglobal.org/spec/lti/claim/roles']
     username = custom_params['user_username']
@@ -107,7 +111,8 @@ def extracting_launch_variables_for_tool_use(request, message_launch):
         user_obj = User.objects.get(username=username)
     except User.DoesNotExist:
         password = ''.join(random.sample(string.ascii_letters, RANDOM_PASSWORD_DEFAULT_LENGTH))
-        user_obj = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+        user_obj = User.objects.create_user(username=username, email=email, password=password, first_name=first_name,
+                                            last_name=last_name)
     user_obj.backend = 'django.contrib.auth.backends.ModelBackend'
     django.contrib.auth.login(request, user_obj)
 
@@ -130,7 +135,6 @@ def extracting_launch_variables_for_tool_use(request, message_launch):
     elif course_details.term_id is not None:
         logger.info(f'Course {course_id} is ready with Data')
         is_course_data_loaded = True
-
 
     myla_globals = {
         "username": username,
@@ -182,11 +186,17 @@ def launch(request):
         return JsonResponse(error_message)
     launch_data_storage = DjangoCacheDataStorage(cache_name='default')
     message_launch = DjangoMessageLaunch(request, tool_conf, launch_data_storage=launch_data_storage)
-    # fetch platform's public key from cache instead of everytime calling the API will speep up the launch process
+    # fetch platform's public key from cache instead of calling the API will speed up the launch process
     cache_ttl = settings.DB_CACHE_CONFIGS['CACHE_TTL']
     cache_lifetime = cache_ttl if cache_ttl else 7200
     message_launch.set_public_key_caching(launch_data_storage, cache_lifetime=cache_lifetime)
-    myla_globals = extracting_launch_variables_for_tool_use(request, message_launch)
+    try:
+       myla_globals = extracting_launch_variables_for_tool_use(request, message_launch)
+    except Exception as e:
+        error_message = {
+            'lti_error_message': f'LTI Launch failed due to {e}'
+        }
+        return JsonResponse(error_message)
     context = {
         'myla_globals': myla_globals
     }
