8d7cddb80ebb8c6a2adfae24ce3a770bb9b96094
chrisrrowland <chris.robert.rowland@gmail.com>
2020-08-14 11:39:36 -0400

    Update dashboard/common/utils.py
    Co-authored-by: Sam Sciolla <35741256+ssciolla@users.noreply.github.com>

--- dashboard/common/utils.py
+++ dashboard/common/utils.py
@@ -91,7 +91,7 @@ def get_myla_globals(current_user):
         "google_analytics_id": google_analytics_id,
         "help_url": help_url,
         "view_help_urls": { 
-            'ra':settings.URL_VIEW_RESOURCES_ACCESSED,
+            'ra': settings.URL_VIEW_RESOURCES_ACCESSED,
             'apv1':settings.URL_VIEW_ASSIGNMENT_PLANNING_V1,
             'ap':settings.URL_VIEW_ASSIGNMENT_PLANNING,
             'gd':settings.URL_VIEW_GRADE_DISTRIBUTION 
