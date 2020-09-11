bdfb9bd8d29c0f4ec2e8d8d7e5a4043e00d1c50b
chrisrrowland <chris.robert.rowland@gmail.com>
2020-09-11 14:36:44 -0400

    Improved message when course not loaded (#1077)
    * Improved message when course not loaded
    
    Additionally allow access to get_course_info if a course is not loaded
    (per null term hack)
    
    * Revert wording change
    
    * Switch to using a single more efficent filter

--- dashboard/rules.py
+++ dashboard/rules.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 import rules, logging
-from dashboard.models import User
+from dashboard.models import Course, User
 
 logger = logging.getLogger(__name__)
 
@@ -20,6 +20,13 @@ def is_admin(self, user):
 @rules.predicate
 def is_enrolled_in_course(user, course):
     return is_enrolled_in_course_id.test(user, course.id)
+
+@rules.predicate
+def course_is_not_loaded(user, course):
+    notLoaded = Course.objects.filter(id=course.id, term__isnull=True).count() == 1
+    if notLoaded:
+        logger.info(f'Permissions course_is_not_loaded: user {user.id} requested access to unloaded course {course.id}')
+    return notLoaded
 
 @rules.predicate(bind=True)
 def is_enrolled_in_course_id(self, user, course_id):
@@ -60,12 +67,13 @@ def is_instructor_in_course_id(self, user, course_id):
     return result
 
 is_admin_or_enrolled_in_course = is_admin | is_enrolled_in_course
+is_admin_or_enrolled_in_course_or_load_pending = is_admin_or_enrolled_in_course | course_is_not_loaded
 is_admin_or_enrolled_in_course_id = is_admin | is_enrolled_in_course_id
 is_admin_or_instructor_in_course = is_admin | is_instructor_in_course
 is_admin_or_instructor_in_course_id = is_admin | is_instructor_in_course_id
 
 # api
-rules.add_perm('dashboard.get_course_info', is_admin_or_enrolled_in_course)
+rules.add_perm('dashboard.get_course_info', is_admin_or_enrolled_in_course_or_load_pending)
 rules.add_perm('dashboard.update_course_info', is_admin_or_instructor_in_course)
 rules.add_perm('dashboard.resource_access_within_week', is_admin_or_enrolled_in_course)
 rules.add_perm('dashboard.grade_distribution', is_admin_or_enrolled_in_course)
