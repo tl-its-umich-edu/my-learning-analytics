9cf7845cf53e8349851f6b0e96163acc6adc510d
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2020-09-14 15:57:51 -0400

    #1066 - keeping it DRY
    Applying DRY principles as suggested by @ssciolla.  This case shows that code to reduce repetition can actually make the code longer. (+19 lines.)

--- dashboard/management/commands/createkeys.py
+++ dashboard/management/commands/createkeys.py
@@ -1,18 +1,27 @@
 import json
 import os
 from datetime import datetime
+from enum import Enum
 
 from Crypto.PublicKey import RSA
+from Crypto.PublicKey.RSA import RsaKey
 from django.core.management.base import BaseCommand, CommandParser
 from jwcrypto.jwk import JWK
 
 
 class Command(BaseCommand):
-    privateKeyFileSuffix = '_private.pem'
-    publicKeyFileSuffix = '_public.pem'
-    jwkFileSuffix = '_public-jwk.json'
     baseNameOption = 'basename'
     timestampFormat = '%Y%m%d%H%M%S'
+    _keyFileBasePathName: str
+
+    class KeyFileType(Enum):
+        JWK = ('JWK', '_public-jwk.json')
+        PRIVATE = ('private key', '_private.pem')
+        PUBLIC = ('public key', '_public.pem')
+
+        def __init__(self, description, fileSuffix):
+            self.description = description
+            self.fileSuffix = fileSuffix
 
     def add_arguments(self, parser: CommandParser):
         parser.add_argument(
@@ -24,41 +33,51 @@ class Command(BaseCommand):
                  f'timestamp of "{self.timestampFormat.replace("%", "%%")}"',
             dest=self.baseNameOption)
 
+    def _writeKeyFile(self, keyFileType: KeyFileType, keyContent: str):
+        keyFileName: str = f'{self.keyFileBasePathName}' \
+                           f'{keyFileType.fileSuffix}'
+        self.stdout.write(
+            f'Writing {keyFileType.description} to file '
+            f'"{keyFileName}"...')
+        with open(keyFileName, 'w') as f:
+            f.writelines((keyContent, '\n'))
+        self.stdout.write('File successfully written.')
+
+    @property
+    def keyFileBasePathName(self):
+        return self._keyFileBasePathName
+
+    @keyFileBasePathName.setter
+    def keyFileBasePathName(self, value: str):
+        self._keyFileBasePathName = value
+
     def handle(self, *args, **options: dict):
-        configDir = os.path.dirname(os.getenv('ENV_FILE', '/secrets/env.json'))
+        configDir: str = os.path.dirname(
+            os.getenv('ENV_FILE', '/secrets/env.json'))
         self.stdout.write('Key files will be written to '
                           f'directory "{configDir}"...')
 
-        keyFileBasePathName = os.path.join(
+        self.keyFileBasePathName = os.path.join(
             configDir, options.get(self.baseNameOption))
 
         self.stdout.write('Generating key...')
-        key = RSA.generate(4096)
+        key: RsaKey = RSA.generate(4096)
 
         self.stdout.write('Preparing private and public key strings...')
-        privateKey = key.exportKey()
-        publicKey = key.publickey().exportKey()
+        privateKey: bytes = key.exportKey()
+        publicKey: bytes = key.publickey().exportKey()
 
-        privateKeyFileName = f'{keyFileBasePathName}{self.privateKeyFileSuffix}'
-        self.stdout.write(
-            f'Writing private key to file "{privateKeyFileName}"...')
-        with open(privateKeyFileName, 'w') as f:
-            f.writelines((privateKey.decode('utf-8'), '\n'))
-
-        publicKeyFileName = f'{keyFileBasePathName}{self.publicKeyFileSuffix}'
-        self.stdout.write(
-            f'Writing public key to file "{publicKeyFileName}"...')
-        with open(publicKeyFileName, 'w') as f:
-            f.writelines((publicKey.decode('utf-8'), '\n'))
-
-        jwk_obj = JWK.from_pem(publicKey)
-        public_jwk = {
+        jwk_obj: JWK = JWK.from_pem(publicKey)
+        public_jwk: dict = {
             **json.loads(jwk_obj.export_public()),
             **{'alg': 'RS256', 'use': 'sig'}
         }
 
-        jwkFileName = f'{keyFileBasePathName}{self.jwkFileSuffix}'
-        self.stdout.write(
-            f'Writing JWK to file "{jwkFileName}"...')
-        with open(jwkFileName, 'w') as f:
-            f.writelines((json.dumps(public_jwk), '\n'))
+        self._writeKeyFile(self.KeyFileType.PRIVATE,
+                           privateKey.decode('utf-8'))
+
+        self._writeKeyFile(self.KeyFileType.PUBLIC,
+                           publicKey.decode('utf-8'))
+
+        self._writeKeyFile(self.KeyFileType.JWK,
+                           json.dumps(public_jwk))
