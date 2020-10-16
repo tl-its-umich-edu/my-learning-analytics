80a01df7467281bf38d6c3838be96f0da7781f2a
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2020-10-16 13:58:22 -0400

    #1054 - remove outdated reference

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -413,7 +413,6 @@ if STUDENT_DASHBOARD_SAML:
 
 if STUDENT_DASHBOARD_LTI:
     LTI_CONFIG = ENV.get('LTI_CONFIG', {})
-    LTI_CONFIG_TEMPLATE = ENV.get('LTI_CONFIG_TEMPLATE', {})
 
 # controls whether Unizin specific features/data is available from the Canvas Data source
 DATA_WAREHOUSE_IS_UNIZIN = ENV.get("DATA_WAREHOUSE_IS_UNIZIN", True)
