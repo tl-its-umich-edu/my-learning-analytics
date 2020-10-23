b1455f56f43c31f100f8cb2bfbf417eb2f29520c
Mr. Lance E Sloan «UMich» <lsloan@umich.edu>
2020-10-23 16:30:24 -0400

    Implement LTI config by URL (#1054) (#1105)
    commit 131061aa7cfc9e55398627616896402eddd1ab1b (HEAD -> 1054-lti-config-url, origin/1054-lti-config-url)
    Author: Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
    Date:   Mon Oct 19 16:04:01 2020 -0400
    
    > #1054 - support for `LTI_CONFIG_TEMPLATE_PATH`
    > To give admin users a way to replace the LTI configuration by URL template, added support for a `LTI_CONFIG_TEMPLATE_PATH` application setting.  It's normally null, which would default to the embedded LTI configuration template.  If not null, it must point to a valid file containing an LTI configuration template.
    
    commit 7bad39e4894a9058dfe38d7f162e9670b9b4ba2c
    Author: Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
    Date:   Mon Oct 19 15:59:36 2020 -0400
    
    > #1054 - fix discovered bug, misc. clean-up
    > While testing my local config, I found that if I didn't have `public_key_file` set in my config and it defaulted to value `/secret/public.key`, I would get an error.  That's because my local config and all the documentation say the directory should be named `secrets`, plural.
    > I was also wanted to fix the clarity and brevity of the code, making `check_if_success_getting_tool_config` → `is_config_valid` and simplifying two-line calls for `LTIError` with single-line `lti_error`.  I feel that `is_config_valid` should be combined with `get_tool_conf`.  Maybe I'll do that soon.
    
    commit 30e83817db0d5f580638e222f86169cd7c023ccd
    Author: Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
    Date:   Fri Oct 16 15:04:05 2020 -0400
    
    > #1054 - better message for LTI config errors
    
    commit 536914f2b2f6b44b43a6a30f810ef1a209677a59
    Author: Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
    Date:   Fri Oct 16 14:21:40 2020 -0400
    
    > #1054 - improved formatting
    
    commit dd69df21f720d738b61856b5dc1e404bf4af7cb8
    Author: Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
    Date:   Fri Oct 16 14:21:11 2020 -0400
    
    > #1054 - removed `recursive_format()` 😢
    > I've archived `recursive_format()` for posteriority at (https://gist.github.com/lsloan/e47ad220846e0e9dc0a561fb3135933e#file-recursiveformat-py).
    > Also updated some formatting and removed the unused `request` argument from `get_jwks()`.
    
    commit 80a01df7467281bf38d6c3838be96f0da7781f2a
    Author: Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
    Date:   Fri Oct 16 13:58:22 2020 -0400
    
    > #1054 - remove outdated reference
    
    commit 943a07da4ad3f82a10a2221cb3c89e7d14f70822
    Author: Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
    Date:   Fri Oct 16 13:57:04 2020 -0400
    
    > #1054 - moved LTI config JSON template to new file
    > Moved the LTI configuration JSON template to a new file and changed the formatting style to one more compatible with JSON.
    
    commit f76e7724789ae7b5b5263a290252b19e4af95b54
    Author: Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
    Date:   Thu Oct 15 16:51:30 2020 -0400
    
    > #1054 - corrected parameter
    > Corrected this as mentioned in code review.  See: https://github.com/tl-its-umich-edu/my-learning-analytics/pull/1105#discussion_r505511939
    
    commit 8aa73b82c66e0b65bbe4a95cfefd4704848ff3f1
    Author: Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
    Date:   Fri Oct 9 12:51:36 2020 -0400
    
    > #1054 - supporting code and config
    
    commit 304f544d57266666cbe5b7b6f157249308bb3ad0
    Author: Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
    Date:   Fri Oct 9 12:48:14 2020 -0400
    
    > #1054 - initial implementation
    > Uses JSON template from the configuration file.

--- dashboard/lti_new.py
+++ dashboard/lti_new.py
@@ -1,7 +1,12 @@
 import logging, string, random
-import django.contrib.auth
+import urllib.parse
+from datetime import datetime
+from typing import Mapping, MutableSequence, Union, Any
 
-from django.http import JsonResponse
+import django.contrib.auth
+from django.contrib.staticfiles import finders
+
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.decorators.http import require_POST
@@ -17,7 +22,6 @@ from pylti1p3.tool_config import ToolConfDict
 from dashboard.models import Course, CourseViewOption, User as MylaUser
 from dashboard.common.db_util import canvas_id_to_incremented_id
 
-
 logger = logging.getLogger(__name__)
 INSTRUCTOR = 'http://purl.imsglobal.org/vocab/lis/v2/membership#Instructor'
 TA = 'http://purl.imsglobal.org/vocab/lis/v2/membership/Instructor#TeachingAssistant'
@@ -25,54 +29,107 @@ COURSE_MEMBERSHIP = 'http://purl.imsglobal.org/vocab/lis/v2/membership'
 DUMMY_CACHE = 'DummyCache'
 
 
-class LTIError:
-    def __init__(self, msg):
-        self.msg = msg
+def lti_error(error_message: Any) -> JsonResponse:
+    """
+    Log an error message and return a JSON response with HTTP status 500.
 
-    def response_json(self):
-        error_message = {
-            'lti_error': f'{self.msg}'
-        }
-        return JsonResponse(error_message, status=500)
+    :param error_message: `Any` type is allowed so objects may be used
+    :return: JsonResponse, with status 500
+    """
+    logger.error(f'LTI error: {error_message}')
+    return JsonResponse({'lti_error': f'{error_message}'}, status=500)
 
 
 def get_tool_conf():
     lti_config = settings.LTI_CONFIG
+
     try:
-        tool_config = ToolConfDict(lti_config)
-    except Exception as e:
-        return e
-    # There should be one key per platform and the name relay on platforms generic domain not institution specific
+        config = ToolConfDict(lti_config)
+    except Exception as error:
+        return error
+
+    # There should be one key per platform
+    # and the name relay on platforms generic domain not institution specific
     platform_domain = list(lti_config.keys())[0]
-    client_id = lti_config[platform_domain][0]['client_id']
-    private_key_file_path = lti_config[platform_domain][0]['private_key_file']
-    public_key_file_path = lti_config[platform_domain][0]['public_key_file']
-    public_key = public_key_file_path if public_key_file_path else '/secret/public.key'
-    private_key = private_key_file_path if private_key_file_path else '/secret/private.key'
+    platform_config = lti_config[platform_domain][0]
+    client_id = platform_config['client_id']
+
     try:
-        private_key_content = open(private_key, 'r').read()
-        public_key_content = open(public_key, 'r').read()
-    except OSError as e:
-        return e
-    tool_config.set_public_key(platform_domain, public_key_content, client_id=client_id)
-    tool_config.set_private_key(platform_domain, private_key_content, client_id=client_id)
-    return tool_config
+        with open(platform_config.get(
+                'private_key_file', '/secrets/private.key'),
+                'r') as private_key_file:
+            config.set_private_key(
+                platform_domain, private_key_file.read(), client_id)
+
+        with open(platform_config.get(
+                'public_key_file', '/secrets/public.key'),
+                'r') as public_key_file:
+            config.set_public_key(
+                platform_domain, public_key_file.read(), client_id)
+
+    except OSError as error:
+        return error
+
+    return config
 
 
-def check_if_success_getting_tool_config(tool_config):
-    if isinstance(tool_config, ToolConfDict):
-        logger.info('Success in fetching LTI tool configuration')
+def is_config_valid(config: ToolConfDict):
+    if isinstance(config, ToolConfDict):
+        logger.info('LTI configuration valid.')
         return True
     else:
-        logger.error(tool_config)
+        logger.error(f'Invalid LTI configuration: "{config}"')
         return False
 
 
-def get_jwks(request):
+def get_jwks(_):
     config = get_tool_conf()
-    if not check_if_success_getting_tool_config(config):
-        return LTIError(config).response_json()
+    if not is_config_valid(config):
+        return lti_error(config)
     return JsonResponse(config.get_jwks())
+
+
+def generate_config_json(request: HttpRequest) -> \
+        Union[HttpResponse, JsonResponse]:
+    config = get_tool_conf()
+    if not is_config_valid(config):
+        return lti_error(f'Invalid LTI configuration: "{config}"')
+
+    parameters = {
+        'timestamp': datetime.now().isoformat(),
+        'host': request.get_host(),
+        'base_url': urllib.parse.urlunsplit(
+            [request.scheme, request.get_host()] + 3 * ['']),
+        'login_url_suffix': reverse('login'),
+        'launch_url_suffix': reverse('launch'),
+        'jwks_url_suffix': reverse('get_jwks'),
+    }
+
+    template_path: str
+    if settings.LTI_CONFIG_TEMPLATE_PATH is not None:
+        template_path = settings.LTI_CONFIG_TEMPLATE_PATH
+    else:
+        template_path = finders.find(
+            'config/lti_config_template.json')
+
+    logger.debug(f'template_path: "{template_path}"')
+
+    template_contents: str
+    try:
+        with open(template_path, 'r') as template_file:
+            template_contents = template_file.read()
+    except OSError as error:
+        return lti_error('Error reading LTI template file '
+                         f'"{template_path}": ({error})')
+
+    config_json: str
+    try:
+        config_json = template_contents % parameters
+    except KeyError as error:
+        return lti_error('Error filling in LTI template from '
+                         f'"{template_path}": ({error})')
+
+    return HttpResponse(config_json, content_type='application/json')
 
 
 def get_cache_config():
@@ -161,12 +218,12 @@ def extract_launch_variables_for_tool_use(request, message_launch):
 @csrf_exempt
 def login(request):
     config = get_tool_conf()
-    if not check_if_success_getting_tool_config(config):
-        return LTIError(config).response_json()
+    if not is_config_valid(config):
+        return lti_error(config)
     target_link_uri = request.POST.get('target_link_uri', request.GET.get('target_link_uri'))
     if not target_link_uri:
         error_message = 'LTI Login failed due to missing "target_link_uri" param'
-        return LTIError(error_message).response_json()
+        return lti_error(error_message)
     CacheConfig = get_cache_config()
     oidc_login = DjangoOIDCLogin(request, config, launch_data_storage=CacheConfig.launch_data_storage)
     return oidc_login.enable_check_cookies().redirect(target_link_uri)
@@ -176,8 +233,8 @@ def login(request):
 @csrf_exempt
 def launch(request):
     config = get_tool_conf()
-    if not check_if_success_getting_tool_config(config):
-        return LTIError(config).response_json()
+    if not is_config_valid(config):
+        return lti_error(config)
     CacheConfig = get_cache_config()
     message_launch = DjangoMessageLaunch(request, config, launch_data_storage=CacheConfig.launch_data_storage)
     if not CacheConfig.is_dummy_cache:
@@ -190,8 +247,7 @@ def launch(request):
     try:
         course_id = extract_launch_variables_for_tool_use(request, message_launch)
     except Exception as e:
-        return LTIError(e).response_json()
+        return lti_error(e)
 
     url = reverse('courses', kwargs={'course_id': course_id})
     return redirect(url)
-
