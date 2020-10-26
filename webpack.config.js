523f9291adf640dc860e2c43dffb5a1914b907ea
pushyami <pushyami@umich.edu>
2020-10-26 09:05:18 -0400

    Adding source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 const path = require('path')
 const BundleTracker = require('webpack-bundle-tracker')
+const isDevelopment = process.env.BABEL_ENV !== 'production'
 
 module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
   entry: path.join(__dirname, 'assets/src/index'),
   output: {
     path: path.join(__dirname, 'assets/dist'),
     filename: '[name]-[hash].js'
   },
-  watchOptions: {
-    ignored: /node_modules/,
-    aggregateTimeout: 600
-  },
+  devtool: isDevelopment ? 'inline-source-map' : 'false',
   module: {
     rules: [
       {
         test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
         loader: 'babel-loader'
       },
       {
