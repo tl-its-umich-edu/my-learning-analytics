346928e5bd246d07dfea2e757ea51422e071d591
Mr. Lance E Sloan «UMich» <lsloan@umich.edu>
2020-12-02 15:09:26 -0500

    Added `getjwk` management command (issue #1065) (#1101)

--- dashboard/management/commands/createkeys.py
+++ dashboard/management/commands/createkeys.py
@@ -1,55 +1,27 @@
-import json
 import os
 from datetime import datetime
-from enum import Enum
 
 from Crypto.PublicKey import RSA
 from Crypto.PublicKey.RSA import RsaKey
 from django.core.management.base import BaseCommand, CommandParser
-from jwcrypto.jwk import JWK
+
+from dashboard.common.keyfileutils import KeyFileUtils
 
 
 class Command(BaseCommand):
+    help = 'Generate a private/public key pair and write them to PEM files.'
     baseNameOption = 'basename'
-    timestampFormat = '%Y%m%d%H%M%S'
-    _keyFileBasePathName: str
-
-    class KeyFileType(Enum):
-        JWK = ('JWK', '_public-jwk.json')
-        PRIVATE = ('private key', '_private.pem')
-        PUBLIC = ('public key', '_public.pem')
-
-        def __init__(self, description, fileSuffix):
-            self.description = description
-            self.fileSuffix = fileSuffix
 
     def add_arguments(self, parser: CommandParser):
         parser.add_argument(
             f'--{self.baseNameOption}',
-            default=datetime.now().strftime(self.timestampFormat),
+            default=datetime.now().strftime(KeyFileUtils.timestampFormat),
             type=str,
             required=False,
-            help='Base file name of the key files generated. Default: '
-                 f'timestamp of "{self.timestampFormat.replace("%", "%%")}"',
+            help='Base file name of the key file(s) generated. '
+                 'Default: timestamp of '
+                 f'"{KeyFileUtils.timestampFormat.replace("%", "%%")}"',
             dest=self.baseNameOption)
-
-    def _writeKeyFile(self, keyFileType: KeyFileType, keyContent: str):
-        keyFileName: str = f'{self.keyFileBasePathName}' \
-                           f'{keyFileType.fileSuffix}'
-        self.stdout.write(
-            f'Writing {keyFileType.description} to file '
-            f'"{keyFileName}"...')
-        with open(keyFileName, 'w') as f:
-            f.writelines((keyContent, '\n'))
-        self.stdout.write('File successfully written.')
-
-    @property
-    def keyFileBasePathName(self):
-        return self._keyFileBasePathName
-
-    @keyFileBasePathName.setter
-    def keyFileBasePathName(self, value: str):
-        self._keyFileBasePathName = value
 
     def handle(self, *args, **options: dict):
         configDir: str = os.path.dirname(
@@ -57,7 +29,8 @@ class Command(BaseCommand):
         self.stdout.write('Key files will be written to '
                           f'directory "{configDir}"...')
 
-        self.keyFileBasePathName = os.path.join(
+        keyFileUtils = KeyFileUtils()
+        keyFileUtils.keyFileBasePathName = os.path.join(
             configDir, options.get(self.baseNameOption))
 
         self.stdout.write('Generating key...')
@@ -67,17 +40,14 @@ class Command(BaseCommand):
         privateKey: bytes = key.exportKey()
         publicKey: bytes = key.publickey().exportKey()
 
-        jwk_obj: JWK = JWK.from_pem(publicKey)
-        public_jwk: dict = {
-            **json.loads(jwk_obj.export_public()),
-            **{'alg': 'RS256', 'use': 'sig'}
-        }
+        result: str
 
-        self._writeKeyFile(self.KeyFileType.PRIVATE,
-                           privateKey.decode('utf-8'))
+        result = keyFileUtils.writeKeyFile(
+            keyFileUtils.KeyFileType.PRIVATE,
+            privateKey.decode('utf-8'))
+        self.stdout.write(result)
 
-        self._writeKeyFile(self.KeyFileType.PUBLIC,
-                           publicKey.decode('utf-8'))
-
-        self._writeKeyFile(self.KeyFileType.JWK,
-                           json.dumps(public_jwk))
+        result = keyFileUtils.writeKeyFile(
+            keyFileUtils.KeyFileType.PUBLIC,
+            publicKey.decode('utf-8'))
+        self.stdout.write(result)
