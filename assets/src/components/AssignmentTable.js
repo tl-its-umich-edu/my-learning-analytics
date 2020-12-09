b3a4d64eca2aae04f9d9d15e6b19d23843ca43ec
Chris Rowland <chris.robert.rowland@gmail.com>
2020-12-09 15:18:54 -0500

    Move indivudual filter logic into its own function

--- assets/src/components/AssignmentTable.js
+++ assets/src/components/AssignmentTable.js
@@ -178,12 +178,23 @@ function AssignmentTable (props) {
     setFiltersAreClear(assignmentNameFilter.trim().length === 0 && assignmentStatusFilterArray.length === 0 && assignmentGroupFilterArray.length === 0)
   }, [assignmentNameFilter, assignmentStatusFilterArray, assignmentGroupFilterArray])
 
+  const matchesNameFilter = (assignment, nameFilter) => {
+    return nameFilter.trim().length === 0 || assignment.name.toUpperCase().includes(nameFilter.toUpperCase())
+  }
+
+  const matchesAssignmentStatusFilter = (assignment, statusArray) => {
+    return statusArray.length === 0 || (statusArray.indexOf(assignmentStatus.GRADED) >= 0 && assignment.graded) || (statusArray.indexOf(assignmentStatus.SUBMITTED) >= 0 && assignment.submitted && !assignment.graded) || (statusArray.indexOf(assignmentStatus.UNSUBMITTED) >= 0 && !(assignment.graded || assignment.submitted))
+  }
+
+  const matchesAssignmentGroupFilter = (assignment, groupArray) => {
+    return groupArray.length === 0 || groupArray.indexOf(assignment.assignmentGroup.name) >= 0
+  }
   useEffect(() => {
     setFilteredAssignments(
       assignments
-        .filter(assignment => assignmentNameFilter.trim().length === 0 || assignment.name.toUpperCase().includes(assignmentNameFilter.toUpperCase()))
-        .filter(assignment => assignmentStatusFilterArray.length === 0 || (assignmentStatusFilterArray.indexOf(assignmentStatus.GRADED) >= 0 && assignment.graded) || (assignmentStatusFilterArray.indexOf(assignmentStatus.SUBMITTED) >= 0 && assignment.submitted && !assignment.graded) || (assignmentStatusFilterArray.indexOf(assignmentStatus.UNSUBMITTED) >= 0 && !(assignment.graded || assignment.submitted)))
-        .filter(assignment => assignmentGroupFilterArray.length === 0 || assignmentGroupFilterArray.indexOf(assignment.assignmentGroup.name) >= 0)
+        .filter(assignment => matchesNameFilter(assignment, assignmentNameFilter))
+        .filter(assignment => matchesAssignmentStatusFilter(assignment, assignmentStatusFilterArray))
+        .filter(assignment => matchesAssignmentGroupFilter(assignment, assignmentGroupFilterArray))
     )
   }, [assignments, assignmentNameFilter, assignmentStatusFilterArray, assignmentGroupFilterArray])
 
