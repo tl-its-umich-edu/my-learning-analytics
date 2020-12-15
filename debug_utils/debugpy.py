58a571cc036b5e18f2a21e9d6a32cb48b9e69de9
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2020-12-15 10:44:48 -0500

    Updates for Codacy: variable names, type hinting

--- debug_utils/debugpy.py
+++ debug_utils/debugpy.py
@@ -1,25 +1,32 @@
-import os, logging
+import os
+import logging
+from typing import Union
+
 from distutils.util import strtobool
 from django.conf import settings
 
 logger = logging.getLogger(__name__)
 # Converts a str to bool, or just returns the bool
-def forcebool(s):
-    if isinstance (s, str):
-        return strtobool(s)
-    return s
+
+
+def forcebool(bool_str: str) -> Union[bool, str]:
+    if isinstance(bool_str, str):
+        return strtobool(bool_str)
+    return bool_str
+
 
 def check_and_enable_debugpy():
     # These could either come from settings or environment
-    DEBUGPY_ENABLE = forcebool(os.getenv('DEBUGPY_ENABLE', getattr(settings, 'DEBUGPY_ENABLE', False)))
-    DEBUGPY_WAIT_FOR_ATTACH = forcebool(os.getenv('DEBUGPY_WAIT_FOR_ATTACH', getattr(settings, 'DEBUGPY_WAIT_FOR_ATTACH', False)))
-    DEBUGPY_ADDRESS = os.getenv('DEBUGPY_REMOTE_ADDRESS',getattr(settings, 'DEBUGPY_REMOTE_ADDRESS', '0.0.0.0'))
-    DEBUGPY_PORT = os.getenv('DEBUGPY_REMOTE_PORT', getattr(settings, 'DEBUGPY_REMOTE_PORT', '5678'))
+    debugpy_enable = forcebool(os.getenv('DEBUGPY_ENABLE', getattr(settings, 'DEBUGPY_ENABLE', False)))
+    debugpy_wait_for_attach = forcebool(os.getenv('DEBUGPY_WAIT_FOR_ATTACH',
+                                                  getattr(settings, 'DEBUGPY_WAIT_FOR_ATTACH', False)))
+    debugpy_address = os.getenv('DEBUGPY_REMOTE_ADDRESS', getattr(settings, 'DEBUGPY_REMOTE_ADDRESS', '0.0.0.0'))
+    debugpy_port = os.getenv('DEBUGPY_REMOTE_PORT', getattr(settings, 'DEBUGPY_REMOTE_PORT', '5678'))
 
-    if DEBUGPY_ENABLE:
+    if debugpy_enable:
         import debugpy
-        debugpy.listen((DEBUGPY_ADDRESS, int(DEBUGPY_PORT)))
-        logger.info('DEBUGPY: Enabled Listen ({0}:{1})'.format(DEBUGPY_ADDRESS, DEBUGPY_PORT))
-        if DEBUGPY_WAIT_FOR_ATTACH:
+        debugpy.listen((debugpy_address, int(debugpy_port)))
+        logger.info('DEBUGPY: Enabled Listen ({0}:{1})'.format(debugpy_address, debugpy_port))
+        if debugpy_wait_for_attach:
             logger.info('DEBUGPY: Waiting for attach...')
             debugpy.wait_for_client()
