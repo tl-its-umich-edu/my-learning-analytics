3728b010e57253f6e34c3c0dbf857050623da11c
Sam Sciolla <ssciolla@umich.edu>
2021-02-04 14:59:54 -0500

    Add first draft of Line/Label refactor

--- assets/src/components/Line.js
+++ assets/src/components/Line.js
@@ -1,37 +1,94 @@
 import React from 'react'
-// import { useDrag } from 'react-dnd'
+
+import Label from './Label'
+import { roundToXDecimals } from '../util/math'
+
+const determineVisualXPercent = (value, maxValue) => {
+  if (value > maxValue) return maxValue
+  if (value < 0) return 0
+  return value / maxValue * 100
+}
+
+const getVerticalStyles = (barHeight, placement) => {
+  const verticalUnit = 25
+  const textHeight = 16
+
+  switch (placement) {
+    case 'up1':
+      return {
+        lineHeight: `${barHeight + verticalUnit}px`,
+        lineMarginTop: `-${verticalUnit}px`,
+        lineMarginBottom: '0',
+        labelZIndex: '3',
+        labelTop: `-${verticalUnit}px`
+      }
+    case 'down1':
+      return {
+        lineHeight: `${barHeight + verticalUnit}px`,
+        lineMarginTop: '0',
+        lineMarginBottom: `-${verticalUnit}px`,
+        labelZIndex: '2',
+        labelTop: `${barHeight + verticalUnit - textHeight}px`
+      }
+    case 'down2':
+      return {
+        lineHeight: `${barHeight + (verticalUnit * 2)}px`,
+        lineMarginTop: '0',
+        lineMarginBottom: `-${verticalUnit}px`,
+        labelZIndex: '1',
+        labelTop: `${barHeight + (verticalUnit * 2) - textHeight}px`
+      }
+    default:
+      return {
+        lineHeight: barHeight,
+        lineMarginTop: '0',
+        lineMarginBottom: '0',
+        labelZIndex: '0',
+        labelTop: '0'
+      }
+  }
+}
 
 function Line (props) {
   const {
-    height,
-    left,
+    value,
+    outOf,
+    barHeight,
     color,
     width = 2,
-    labelPlacement // Value should be one of 'up1', 'down1', and 'down2'.  1 or 2 represents an approximation of lines of text of offset up or down.
+    placement, // Value should be one of 'up1', 'down1', and 'down2'.  1 or 2 represents an approximation of lines of text of offset up or down.
+    label = undefined
   } = props
 
-  const lineHeight = labelPlacement === 'up1' || labelPlacement === 'down1'
-    ? height + 25
-    : labelPlacement === 'down2'
-      ? height + 42
-      : height + 2
+  const verticalStyles = getVerticalStyles(barHeight, placement)
 
   return (
-    <div
-      style={{
-        position: 'absolute',
-        display: 'inline-block',
-        width: `${width}px`,
-        backgroundColor: color,
-        height: `${lineHeight}px`,
-        left,
-        zIndex: 1,
-        marginTop: labelPlacement === 'up1' ? '-25px' : 0,
-        marginBottom: labelPlacement === 'down1' ||
-          labelPlacement === 'down2' ? '-25px' : 0
-      }}
-      // ref={drag}
-    />
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          display: 'inline-block',
+          width: `${width}px`,
+          backgroundColor: color,
+          height: verticalStyles.lineHeight,
+          left: `${determineVisualXPercent(value, outOf)}%`,
+          zIndex: 1,
+          marginTop: verticalStyles.lineMarginTop,
+          marginBottom: verticalStyles.lineMarginBottom
+        }}
+      />
+      {
+        label && (
+          <Label
+            labelText={`${label}: ${roundToXDecimals(value, 1)}%`}
+            color={color}
+            visualXPercent={determineVisualXPercent(value, outOf)}
+            top={verticalStyles.labelTop}
+            zIndex={verticalStyles.labelZIndex}
+          />
+        )
+      }
+    </>
   )
 }
 
