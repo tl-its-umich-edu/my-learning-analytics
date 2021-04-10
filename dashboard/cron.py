423d4646e0657ec8be6762720b1e21a35eeb0216
Mr. Lance E Sloan «UMich» <lsloan-github.com@umich.edu>
2021-04-09 21:50:00 -0400

    corrected variable name
    The variable contains SQL, not URL.

--- dashboard/cron.py
+++ dashboard/cron.py
@@ -506,7 +506,7 @@ class DashboardCronJob(CronJobBase):
         # loop through multiple course ids
         # filter out not released grades (submission_dim.posted_at date is not null) and partial grades (submission_dim.workflow_state != 'graded')
         for data_warehouse_course_id in self.valid_locked_course_ids:
-            submission_url = f"""with sub_fact as (select submission_id, assignment_id, course_id, user_id, global_canvas_id, published_score from submission_fact sf join user_dim u on sf.user_id = u.id where course_id = '{data_warehouse_course_id}'),
+            submission_sql = f"""with sub_fact as (select submission_id, assignment_id, course_id, user_id, global_canvas_id, published_score from submission_fact sf join user_dim u on sf.user_id = u.id where course_id = '{data_warehouse_course_id}'),
                 enrollment as (select  distinct(user_id) from enrollment_dim where course_id = '{data_warehouse_course_id}' and workflow_state='active' and type = 'StudentEnrollment'),
                 sub_with_enroll as (select sf.* from sub_fact sf join enrollment e on e.user_id = sf.user_id),
                 submission_time as (select sd.id, sd.submitted_at, sd.graded_at, sd.posted_at at time zone 'utc' at time zone '{settings.TIME_ZONE}' as grade_posted_local_date, sd.workflow_state as submission_workflow_state from submission_dim sd join sub_fact suf on sd.id=suf.submission_id),
@@ -515,7 +515,7 @@ class DashboardCronJob(CronJobBase):
                 all_assign_sub as (select submission_id AS id, assignment_id AS assignment_id, course_id, global_canvas_id AS user_id, (case when (grade_posted_local_date is null or submission_workflow_state != 'graded') then null else round(published_score,1) end) AS score, submitted_at AS submitted_at, graded_at AS graded_date, grade_posted_local_date from assign_sub_time order by assignment_id)
                 select f.*, f1.avg_score from all_assign_sub f join (select assignment_id, round(avg(score),1) as avg_score from all_assign_sub group by assignment_id) as f1 on f.assignment_id = f1.assignment_id;
             """
-            status += util_function(data_warehouse_course_id, submission_url,'submission')
+            status += util_function(data_warehouse_course_id, submission_sql, 'submission')
 
         return status
 
