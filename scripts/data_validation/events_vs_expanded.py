83c749203c060faf48781f223afa70adc3d9815e
zqian <zqian@umich.edu>
2021-07-28 23:37:21 -0400

    issue-1278 changes based on code review comments

--- scripts/data_validation/events_vs_expanded.py
+++ scripts/data_validation/events_vs_expanded.py
@@ -13,9 +13,6 @@ def run_canvas_query(query_array, query_params):
     query_string = ''.join(query_array)
     print(query_string)
 
-    # BQ Total Bytes Billed to report to status
-    total_bytes_billed = 0
-
     job_config = bigquery.QueryJobConfig()
     job_config.query_parameters = query_params
 
@@ -24,9 +21,6 @@ def run_canvas_query(query_array, query_params):
         query_string, location='US', job_config=job_config)
     # This is the call that could result in an exception
     df = bq_job.result().to_dataframe()
-
-    # print(df.to_string())
-    total_bytes_billed += bq_job.total_bytes_billed
 
     return df
 
@@ -80,131 +74,20 @@ def main():
             'time_limit', 'STRING', TIME_LIMIT)
     ]
 
-    # compare the Canvas query
-    canvas_expanded_query_array = [
-        "SELECT 'canvas' AS resource_type, ",
-        "REGEXP_EXTRACT(object.id, r'.*:(.*)') AS resource_id, ",
-        "CAST(REGEXP_EXTRACT(membership.id, r'.*:(.*)') AS INT64) AS user_id, ",
-        "cast(null as string) AS user_login_name, ",
-        "CAST(REGEXP_EXTRACT(`group`.id, r'.*:(.*)') AS INT64) AS course_id, ",
-        "COALESCE( ",
-        "JSON_EXTRACT_SCALAR(object.extensions, '$[\\'com.instructure.canvas\\'][asset_name]'), ",
-        "JSON_EXTRACT_SCALAR(object.extensions, '$[\\'com.instructure.canvas\\'][filename]'), ",
-        "object.name, ",
-        "'attachment' ",
-        ") as name, ",
-        "datetime(EVENT_TIME) as access_time ",
-        "FROM event_store.expanded ",
-        "where ",
-        "JSON_EXTRACT_SCALAR(ed_app.json, '$.id') IN UNNEST(['http://m.canvas.umich.edu/', 'http://umich.instructure.com/']) ",
-        "and type = 'NavigationEvent' ",
-        "and STARTS_WITH(object.id, 'urn:instructure:canvas:attachment') ",
-        "and action = 'NavigatedTo' ",
-        "and membership.id is not null ",
-        "and REGEXP_EXTRACT(`group`.id, r'.*:(.*)') IN UNNEST(@course_ids) ",
-        "and event_time > @time_limit ",
-        "order by resource_id, user_id, access_time "
-    ]
-    canvas_events_query_array = [
-        "SELECT 'canvas' AS resource_type, ",
-        "CAST(SUBSTR(JSON_EXTRACT_SCALAR(event, '$.object.id'), 35) AS STRING) AS resource_id, ",
-        "CAST(SUBSTR(JSON_EXTRACT_SCALAR(event, '$.membership.member.id'), 29) AS INT64) AS user_id, ",
-        "cast(null as string) AS user_login_name,",
-        "CAST(SUBSTR(JSON_EXTRACT_SCALAR(event, '$.group.id'), 31) AS INT64) AS course_id, ",
-        "COALESCE(",
-        "JSON_EXTRACT_SCALAR(event, '$.object.extensions[\\'com.instructure.canvas\\'].asset_name'), ",
-        "JSON_EXTRACT_SCALAR(event, '$.object.extensions[\\'com.instructure.canvas\\'].filename'), ",
-        "JSON_EXTRACT_SCALAR(event, '$.object.name'), ",
-        "'attachment'",
-        ") as name, ",
-        "datetime(EVENT_TIME) as access_time ",
-        "FROM event_store.events ",
-        "where JSON_EXTRACT_SCALAR(event, '$.edApp.id') IN ",
-        "UNNEST(['http://m.canvas.umich.edu/'   , 'http://umich.instructure.com/'   ]) ",
-        "and type = 'NavigationEvent' ",
-        "and SUBSTR(JSON_EXTRACT_SCALAR(event, '$.object.id'), 24, 10) = 'attachment' ",
-        "and JSON_EXTRACT_SCALAR(event, '$.action') = 'NavigatedTo' ",
-        "and JSON_EXTRACT_SCALAR(event, '$.membership.member.id') is not null ",
-        "and SUBSTR(JSON_EXTRACT_SCALAR(event, '$.group.id'), 31) IN UNNEST(@course_ids) ",
-        "and event_time > @time_limit ",
-        "order by resource_id, user_id, access_time"
-    ]
-    compare_expanded_vs_events_df(
-        canvas_expanded_query_array, canvas_events_query_array, query_params)
-
-    # compare the lecture capture query
-    leccap_expanded_query_array = [
-        "select 'leccap' AS resource_type, ",
-        "REGEXP_EXTRACT(object.id, r'.*:(.*)') AS resource_id, ",
-        "@canvas_data_id_increment + CAST(JSON_EXTRACT_SCALAR(federated_session_json, '$.messageParameters.custom_canvas_user_id') AS INT64) AS user_id, ",
-        "cast(null as string) AS user_login_name,",
-        "@canvas_data_id_increment + CAST(JSON_EXTRACT_SCALAR(federated_session_json, '$.messageParameters.custom_canvas_course_id') AS INT64) AS course_id, ",
-        "object.name as name, ",
-        "datetime(EVENT_TIME) as access_time ",
-        "FROM event_store.expanded ",
-        "where ed_app.id = 'https://leccap.engin.umich.edu/#applicationName=Lecture+Capture'  ",
-        "and type = 'MediaEvent' and action = 'Started' ",
-        "and JSON_EXTRACT_SCALAR(federated_session_json, '$.messageParameters.custom_canvas_course_id') is not null ",
-        "and JSON_EXTRACT_SCALAR(federated_session_json, '$.messageParameters.custom_canvas_course_id') IN UNNEST(@course_ids_short) ",
-        "and event_time > @time_limit ",
-        "order by resource_id, user_id, access_time"
-    ]
-    leccap_events_query_array = [
-        "select 'leccap' AS resource_type, ",
-        "CAST(SUBSTR(JSON_EXTRACT_SCALAR(event, '$.object.id'), 48) AS STRING) AS resource_id, ",
-        "@canvas_data_id_increment + CAST(JSON_EXTRACT_SCALAR(event, '$.federatedSession.messageParameters.custom_canvas_user_id') AS INT64) AS user_id, ",
-        "cast(null as string) AS user_login_name,",
-        "@canvas_data_id_increment + CAST(JSON_EXTRACT_SCALAR(event, '$.federatedSession.messageParameters.custom_canvas_course_id') AS INT64) AS course_id, ",
-        "JSON_EXTRACT_SCALAR(event, '$.object.name') as name, ",
-        "datetime(EVENT_TIME) as access_time ",
-        "FROM event_store.events ",
-        "where JSON_EXTRACT_SCALAR(event, '$.edApp') = 'https://leccap.engin.umich.edu/#applicationName=Lecture+Capture'   ",
-        "and type = 'MediaEvent' and JSON_EXTRACT_SCALAR(event, '$.action') = 'Started' ",
-        "and JSON_EXTRACT_SCALAR(event, '$.federatedSession.messageParameters.custom_canvas_course_id') is not null ",
-        "and JSON_EXTRACT_SCALAR(event, '$.federatedSession.messageParameters.custom_canvas_course_id') IN UNNEST(@course_ids_short) "
-        "and event_time > @time_limit ",
-        "order by resource_id, user_id, access_time"
-    ]
-    compare_expanded_vs_events_df(
-        leccap_expanded_query_array, leccap_events_query_array, query_params)
-
-    # compare MiVideo query
-    mivideo_expanded_query_array = [
-        "SELECT 'mivideo' AS resource_type, ",
-        "replace(object.id, 'https://aakaf.mivideo.it.umich.edu/caliper/info/media/' , '') AS resource_id, ",
-        "cast(-1 as INT64) AS user_id, ",
-        "replace(actor.id, 'https://aakaf.mivideo.it.umich.edu/caliper/info/user/' , '') AS user_login_name, ",
-        "@canvas_data_id_increment + CAST(JSON_EXTRACT_SCALAR(object.extensions, '$.kaf:course_id') AS INT64) AS course_id, ",
-        "object.name AS name, ",
-        "datetime(EVENT_TIME) AS access_time ",
-        "FROM event_store.expanded WHERE ",
-        "ed_app.id = 'https://aakaf.mivideo.it.umich.edu/caliper/info/app/KafEdApp' ",
-        "AND TYPE = 'MediaEvent'",
-        "AND action = 'Started'",
-        "AND JSON_EXTRACT_SCALAR(object.extensions, '$.kaf:course_id') IN UNNEST(@course_ids_short) ",
-        "and event_time > @time_limit ",
-        "order by resource_id, user_id, access_time"
-    ]
-    mivideo_events_query_array = [
-        "SELECT 'mivideo' AS resource_type, ",
-        "replace(JSON_EXTRACT_SCALAR(event, '$.object.id'), 'https://aakaf.mivideo.it.umich.edu/caliper/info/media/'  , '') AS resource_id, ",
-        "cast(-1 as INT64) AS user_id,",
-        "replace(JSON_EXTRACT_SCALAR(event, '$.actor.id'), 'https://aakaf.mivideo.it.umich.edu/caliper/info/user/'  , '') AS user_login_name, ",
-        "@canvas_data_id_increment + CAST(JSON_EXTRACT_SCALAR(event, '$.object.extensions.kaf:course_id') AS INT64) AS course_id, ",
-        "JSON_EXTRACT_SCALAR(event, '$.object.name') AS name,",
-        "datetime(EVENT_TIME) AS access_time ",
-        "FROM event_store.events WHERE ",
-        "COALESCE(JSON_EXTRACT_SCALAR(event, '$.edApp.id'), JSON_EXTRACT_SCALAR(event, '$.edApp')) = 'https://aakaf.mivideo.it.umich.edu/caliper/info/app/KafEdApp' ",
-        "AND TYPE = 'MediaEvent' ",
-        "AND JSON_EXTRACT_SCALAR(event, '$.action') = 'Started' ",
-        "AND JSON_EXTRACT_SCALAR(event, '$.object.extensions.kaf:course_id') IN UNNEST(@course_ids_short) ",
-        "and event_time > @time_limit ",
-        "order by resource_id, user_id, access_time"
-    ]
-
-    # compare mivideo
-    compare_expanded_vs_events_df(
-        mivideo_expanded_query_array, mivideo_events_query_array, query_params)
+    # from the configuration variable, load the queries based on UDP expanded vs events table
+    # run queries and compare the returned dataframes
+    expanded_vs_events_queries_json = ENV["EXPANDED_VS_EVENTS_QUERIES"]
+    for event_type in expanded_vs_events_queries_json:
+        print(event_type)
+        expanded_query_array = []
+        events_query_array = []
+        for attribute, value in expanded_vs_events_queries_json[event_type].items():
+            if attribute == "expanded_query_array":
+                expanded_query_array = value
+            elif attribute == "events_query_array":
+                events_query_array = value
+        compare_expanded_vs_events_df(
+            expanded_query_array, events_query_array, query_params)
 
 
 if __name__ == "__main__":
