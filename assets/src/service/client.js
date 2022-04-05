c96a1e9cd67c598d58e50b06d44d1a773afcad8b
Sam Sciolla <ssciolla@umich.edu>
2022-04-05 13:55:04 -0400

    Make URI relative; remove trailing comma

--- assets/src/service/client.js
+++ assets/src/service/client.js
@@ -2,11 +2,11 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 import Cookie from 'js-cookie'
 
 export default new ApolloClient({
-  uri: 'http://localhost:5001/graphql',
+  uri: '/graphql',
   cache: new InMemoryCache(),
   headers: {
     Accept: 'application/json',
     'X-Requested-With': 'XMLHttpRequest',
     'X-CSRFToken': Cookie.get('csrftoken')
-  },
+  }
 })
