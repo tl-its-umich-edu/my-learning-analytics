29908a4f6a80f53daabb208d2ccd186608154cbf
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2022-08-25 15:51:40 -0400

    Fixes #1396 - Check if deployment ID claim is missing and return an error even if validation is disabled

--- dashboard/lti_new.py
+++ dashboard/lti_new.py
@@ -38,7 +38,8 @@ DUMMY_CACHE = 'DummyCache'
 # do not require deployment ids if LTI_CONFIG_DISABLE_DEPLOYMENT_ID_VALIDATION is true
 class ExtendedDjangoMessageLaunch(DjangoMessageLaunch):
     def validate_deployment(self):
-        if settings.LTI_CONFIG_DISABLE_DEPLOYMENT_ID_VALIDATION:
+        # Check the deployment id is set otherwise the code will raise an exception
+        if settings.LTI_CONFIG_DISABLE_DEPLOYMENT_ID_VALIDATION and self._get_deployment_id():
             return self
 
         return super().validate_deployment()
