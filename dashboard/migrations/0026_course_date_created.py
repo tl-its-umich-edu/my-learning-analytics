3b344fabd049c27ad0f157fc9fc5f6eae9b89475
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2023-09-11 15:41:28 -0400

    Setting default values of date_created to null

--- dashboard/migrations/0026_course_date_created.py
+++ dashboard/migrations/0026_course_date_created.py
@@ -10,10 +10,16 @@ class Migration(migrations.Migration):
         ('dashboard', '0025_remove_courseviewoption_show_assignment_planning_v1'),
     ]
 
+    # Initially set this to null but alter it to default to the current date
     operations = [
         migrations.AddField(
             model_name='course',
-            name='dated_created',
-            field=models.DateTimeField(default=datetime.datetime.now, verbose_name='Created Date'),
+            name='date_created',
+            field=models.DateTimeField(default=None, null=True, blank=True, verbose_name='Date course was created'),
+        ),
+        migrations.AlterField(
+            model_name='course',
+            name='date_created',
+            field=models.DateTimeField(blank=True, default=datetime.datetime.now, null=True, verbose_name='Date course was created'),
         ),
     ]
