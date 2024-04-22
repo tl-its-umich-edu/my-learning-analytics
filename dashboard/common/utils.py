92af7a752aa2c84c1ea3f42bdcc5a66f7fc69be8
Niurca Quirarte <niurcaq@umich.edu>
2024-04-22 16:41:40 -0400

    changes to issue 1358 regarding .git extension

--- dashboard/common/utils.py
+++ dashboard/common/utils.py
@@ -13,9 +13,11 @@ logger = logging.getLogger(__name__)
 def format_github_url_using_https(github_url: str):
     ssh_base = "git@"
     https_base = "https://"
-    # If the URL is formatted for SSH, convert, otherwise, do nothing
+    # If the URL is formatted for SSH, convert, otherwise, replace .git extension with ""
     if ssh_base == github_url[:len(ssh_base)]:
         github_url = github_url.replace(":", "/").replace(".git", "").replace(ssh_base, https_base)
+    else:
+        github_url = github_url.replace(".git", "")
     return github_url
 
 
