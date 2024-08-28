07a558dc07ab9dd4f077967e275eaccc2a92fec7
Pushyami Gundala <pushyami@umich.edu>
2024-08-28 15:34:35 -0400

    #1583 cleaning up super user referance

--- dashboard/common/utils.py
+++ dashboard/common/utils.py
@@ -84,7 +84,7 @@ def get_myla_globals(request):
     google_analytics_id = ""
     course_id = get_course_id_from_request_url(request.path)
 
-    is_superuser = current_user.is_staff
+    is_superuser = current_user.is_superuser
     if current_user.is_authenticated:
         username = current_user.get_username()
         user_courses_info = db_util.get_user_courses_info(username, course_id)
